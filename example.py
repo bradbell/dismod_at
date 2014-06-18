d635d15c2e70859031e05a19d141d000b6a3b4d2
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-06-18 17:14:54 +0000

    doc.omh: change copyright to same format as current bin/add_copyright.sh. example.py: add copyright.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@9588 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- example.py
+++ example.py
@@ -1,4 +1,14 @@
 #! /usr/bin/env python
+# $Id$
+#  --------------------------------------------------------------------------
+# dismod_at: Estimating Disease Rate Estimation as Functions of Age and Time
+#           Copyright (C) 2014-14 University of Washington
+#              (Bradley M. Bell bradbell@uw.edu)
+# 
+# This program is distributed under the terms of the 
+# 	     GNU Affero General Public License version 3.0 or later
+# see http://www.gnu.org/licenses/agpl.txt
+# -------------------------------------------------------------------------- */
 import example
 example.area_table()
 example.create_table()
