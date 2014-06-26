2aa6f7cd18ad75ef0008149ad3c8e6a7941af720
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-06-26 06:12:14 +0000

    get_started.py: create smooth_grid and smooth tables. input.omh: move smooth_grid to this level. smooth_table.omh: move smooth_grid to input.omh.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@9748 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- example/get_started.py
+++ example/get_started.py
@@ -166,9 +166,10 @@ def get_started() :
 		'text',           # density
 		'real'            # eta
 	]
-	uniform_01 = 1
+	uniform_01_id = 1
+	zero_id       = 2
 	row_list = [ [ 
-		uniform_01,       # like_id 
+		uniform_01_id,    # like_id 
 		'uniform_01',     # like_name	
 		0.0,              # lower	
 		1.0,              # upper	
@@ -176,9 +177,60 @@ def get_started() :
 		None,             # std	    (substititue for infinity for sql)
 		'gaussian',       # density (not used because std = infinity)
 		1e-6              # eta     (not used because std = infinity)
+	], [
+		zero_id,          # like_id 
+		'zero',           # like_name	
+		0.0,              # lower	
+		0.0,              # upper	
+		0.0,              # mean	
+		None,             # std	    (substititue for infinity for sql)
+		'gaussian',       # density (not used because std = infinity)
+		1e-6              # eta     (not used because std = infinity)
 	] ]
 	tbl_name = 'like'
 	dismod_at.create_table(connection, tbl_name, col_name, col_type, row_list)
 	# -----------------------------------------------------------------------
+	# create smooth_grid table
+	ptype    = 'integer primary key'
+	constant_id = 0
+	col_name = [ 'smooth_grid_id', 'smooth_grid_name' ]
+	col_type = [ ptype,            'text'             ]
+	row_list = [ [ constant_id,    'constant'      ]  ]
+	tbl_name = 'smooth_grid'
+	dismod_at.create_table(connection, tbl_name, col_name, col_type, row_list)
+	# ----------------------------------------------------------------------- 
+	# smooth table column names
+	col_name = [
+		'smooth_id', 
+		'smooth_grid_id', 
+		'age',  
+		'time',  
+		'value_like_id',
+		'dage_like_id',
+		'dtime_like_id',
+	]
+	col_type = [
+		ptype,      # smooth_id
+		'integer',  # smooth_grid_id
+		'real',     # age
+		'real',     # time
+		'integer',  # value_like_id
+		'integer',  # dage_like_id
+		'integer',  # dtime_like_id
+	]
+	row_list = list()
+	smooth_grid_id = 1
+	row_list  = [ [
+		None,            # smooth_id
+		smooth_grid_id,  # smooth_grid_id 
+		50.0,            # age
+		2000.0,          # time
+		uniform_01_id,   # value_like_id
+		zero_id,         # dage_like_id
+		zero_id          # dtime_like_id
+	] ]
+	tbl_name = 'smooth'
+	dismod_at.create_table(connection, tbl_name, col_name, col_type, row_list)
+	# ----------------------------------------------------------------------- 
 	print('get_started: OK')
 # END PYTHON
