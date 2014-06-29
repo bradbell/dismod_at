5c7a92e318d6c930e60f426acdd4ce0553b1e4e8
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-06-29 14:56:37 +0000

    example_c.sh:  request for column information fails for unknow reason.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@9780 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- bin/example_c.sh
+++ bin/example_c.sh
@@ -42,6 +42,20 @@ cat << EOF > example.c
 # include <stdio.h>
 #include <sqlite3.h>
 
+# if 0
+int sqlite3_table_column_metadata(
+  sqlite3 *db,                /* Connection handle */
+  const char *zDbName,        /* Database name or NULL */
+  const char *zTableName,     /* Table name */
+  const char *zColumnName,    /* Column name */
+  char const **pzDataType,    /* OUTPUT: Declared data type */
+  char const **pzCollSeq,     /* OUTPUT: Collation sequence name */
+  int *pNotNull,              /* OUTPUT: True if NOT NULL constraint exists */
+  int *pPrimaryKey,           /* OUTPUT: True if column part of PK */
+  int *pAutoinc               /* OUTPUT: True if column is auto-increment */
+);
+# endif
+
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
   for(i=0; i<argc; i++){
@@ -55,6 +69,7 @@ int main(int argc, char **argv){
   sqlite3* db;
   char*    zErrMsg = 0;
   int      rc;
+  int      j;
 
   rc = sqlite3_open("example.db", &db);
   if( rc ){
@@ -62,7 +77,36 @@ int main(int argc, char **argv){
       sqlite3_close(db);
       return(1);
   }
-  rc = sqlite3_exec(db, "select * from mytable", callback, 0, &zErrMsg);
+# if 0
+  // For some unknown reason: this request generates the erorr message 
+  //  SQL error: no such table column: mytable.one
+  const char *column_name[] = { "one", "two" };
+  for(j = 0; j < 2; j++)
+  {  const char *zDataType;
+     const char *zCollSeq;
+     int NotNull;
+     int PrimaryKey;
+     int Autoinc;
+     rc = sqlite3_table_column_metadata(
+      db,
+      "example", 
+      "mytable", 
+      column_name[j], 
+      &zDataType, 
+      &zCollSeq, 
+      &NotNull, 
+      &PrimaryKey, 
+      &Autoinc
+    );
+    if( rc ){
+      fprintf( stderr, "SQL error: %s\n", sqlite3_errmsg(db) );
+      sqlite3_close(db);
+      return(1);
+    }
+    printf("name = %s, type = %s\n", column_name[j], zDataType);
+  }
+# endif
+  rc = sqlite3_exec(db, "select one,two from mytable", callback, 0, &zErrMsg);
   if( rc!=SQLITE_OK ){
     fprintf(stderr, "SQL error: %s\n", zErrMsg);
     sqlite3_free(zErrMsg);
