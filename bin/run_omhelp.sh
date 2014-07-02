ab71878d743a53a794a9b3cf541ada8222d63bcc
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-07-02 19:04:05 +0000

    Split developer documentation into separate web site.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@9814 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- bin/run_omhelp.sh
+++ bin/run_omhelp.sh
@@ -14,10 +14,15 @@ then
 	echo 'bin/run_omhelp.sh must be run from its parent directory'
 	exit 1
 fi
-if [ -e 'doc' ]
-then
-	rm -r doc
-fi
-mkdir doc
-cd doc
-omhelp ../doc.omh -debug -noframe -xml
+top_source_dir=''
+for dir in doc devel
+do
+	if [ -e "$dir" ]
+	then
+		rm -r $dir
+	fi
+	mkdir $dir
+	cd $dir
+	top_source_dir="../$top_source_dir"
+	omhelp $top_source_dir/$dir.omh -debug -noframe -xml
+done
---
+++ cpp/get_input_data.cpp
@@ -0,0 +1,20 @@
+// $Id$
+/* --------------------------------------------------------------------------
+dismod_at: Estimating Disease Rate Estimation as Functions of Age and Time
+          Copyright (C) 2014-14 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the 
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+/*
+$begin get_input_data$$
+
+$section Get the dismod_at Input Data$$
+
+$head Syntax$$
+
+
+$end
+*/
