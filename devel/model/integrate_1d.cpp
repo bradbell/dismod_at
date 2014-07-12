5bcbc8601d1a87b9957a7446e0d2e3da590f29c6
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-07-12 13:37:17 +0000

    model: setting from bin/svn_ignore.sh. integrate_1d.cpp: map _1 -> _2 and _0 -> _1. table: setting from bin/svn_ignore.sh.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@9995 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- devel/model/integrate_1d.cpp
+++ devel/model/integrate_1d.cpp
@@ -14,17 +14,17 @@ $icode%c% = dismod_at::integrate_1d(%q%, %r%, %w%)%$$
 $head Purpose$$
 Define the functions
 $latex \[
-	v(s) = ( q_1 - q_0 )^{-1} [ ( q_1 - s ) v_0 + ( s - q_0 ) v_1 ] 
+	v(s) = ( q_2 - q_1 )^{-1} [ ( q_2 - s ) v_1 + ( s - q_1 ) v_2 ] 
 \] $$
 $latex \[
-	w(s) = ( r_1 - r_0 )^{-1} [ ( r_1 - s ) w_0 + ( s - r_0 ) w_1 ] 
+	w(s) = ( r_2 - r_1 )^{-1} [ ( r_2 - s ) w_1 + ( s - r_1 ) w_2 ] 
 \] $$
-We wish to compute the coefficients $latex c_0, c_1$$ such that
+We wish to compute the coefficients $latex c_1, c_2$$ such that
 $latex \[
-I( v_0, v_1 ) = \int_{r(0)}^{r(1)} v(s) w(s) \; \B{d} s = c_0 v_0 + c_1 v_1
+I( v_1, v_2 ) = \int_{r(0)}^{r(1)} v(s) w(s) \; \B{d} s = c_1 v_1 + c_2 v_2
 \] $$
-where the coefficients $latex c_0, c_1$$ do not depend on the value of 
-$latex v_0$$ or $latex v_1$$.
+where the coefficients $latex c_1, c_2$$ do not depend on the value of 
+$latex v_1$$ or $latex v_2$$.
 
 $head Arguments$$
 For $icode x$$ in the set of arguments
@@ -43,89 +43,89 @@ $codei%
 	std::pair<double, double> %c%
 %$$
 with the following identification;
-$latex c_0 = $$ $icode%c%.first%$$ and 
-$latex c_1 = $$ $icode%c%.second%$$.
+$latex c_1 = $$ $icode%c%.first%$$ and 
+$latex c_2 = $$ $icode%c%.second%$$.
 
 $head Method$$
-Using the notation $latex d = r_1 - r_0$$,
+Using the notation $latex d = r_2 - r_1$$,
 the integral is given by
 $latex \[
-I( v_0, v_1 ) = \int_0^d v(s + r_1) w(s + r_1) \; \B{d} s 
+I( v_1, v_2 ) = \int_0^d v(s + r_2) w(s + r_2) \; \B{d} s 
 \] $$
 Using the fact that the functions 
 $latex v(s)$$ and $latex w(s)$$ are linear, we have
 $latex \[
-I( v_0, v_1) =  d^{-2} \int_0^d 
-	[ ( w_1 - w_0 ) s + w_0 d ] 
-		( [ v( r_1 ) - v( r_0 ) ] s + v( r_0 ) d )
+I( v_1, v_2) =  d^{-2} \int_0^d 
+	[ ( w_2 - w_1 ) s + w_1 d ] 
+		( [ v( r_2 ) - v( r_1 ) ] s + v( r_1 ) d )
 			\; \B{d} s
 \] $$
 Grouping terms that have the same order in $latex s$$, we obtain
 $latex \[
-I( v_0, v_1) =  d^{-2} \int_0^d  \left(
-	[ ( w_1 - w_0 ) [ v( r_1 ) - v( r_0) ]  s^2 
+I( v_1, v_2) =  d^{-2} \int_0^d  \left(
+	[ ( w_2 - w_1 ) [ v( r_2 ) - v( r_1) ]  s^2 
 	+
-	w_0 d [ v( r_1 ) - v( r_0) ]  s 
+	w_1 d [ v( r_2 ) - v( r_1) ]  s 
 	+
-	v( r_0 ) d ( w_1 - w_0 )  s 
+	v( r_1 ) d ( w_2 - w_1 )  s 
 	+
-	w_0 d v( r_0 ) d
+	w_1 d v( r_1 ) d
 \right) \; \B{d} s
 \] $$
 Doing the integration we have
 $latex \[
-d^{-1} I( v_0, v_1) =  
-	( w_1 - w_0 ) [ v( r_1 ) - v( r_0) ] / 3
+d^{-1} I( v_1, v_2) =  
+	( w_2 - w_1 ) [ v( r_2 ) - v( r_1) ] / 3
 	+
-	w_0 [ v( r_1 ) - v( r_0) ] / 2
+	w_1 [ v( r_2 ) - v( r_1) ] / 2
 	+
-	v( r_0 ) ( w_1 - w_0 )  / 2
+	v( r_1 ) ( w_2 - w_1 )  / 2
 	+
-	w_0 v( r_0 ) 
+	w_1 v( r_1 ) 
 \] $$
 $latex \[
-d^{-1} I( v_0, v_1) =  
-	( w_1 - w_0 ) [ v( r_1 ) - v( r_0) ] / 3
+d^{-1} I( v_1, v_2) =  
+	( w_2 - w_1 ) [ v( r_2 ) - v( r_1) ] / 3
 	+
-	[ w_0 v( r_1 ) +  v( r_0 ) w_1 ] / 2
+	[ w_1 v( r_2 ) +  v( r_1 ) w_2 ] / 2
 \] $$
 $latex \[
-d^{-1} I( v_0, v_1) =  
-	[ w_1 / 2 - ( w_1 - w_0 ) / 3 ] v( r_0 )
+d^{-1} I( v_1, v_2) =  
+	[ w_2 / 2 - ( w_2 - w_1 ) / 3 ] v( r_1 )
 	+
-	[ w_0 / 2 + ( w_1 - w_0 ) / 3 ] v( r_1 )
+	[ w_1 / 2 + ( w_2 - w_1 ) / 3 ] v( r_2 )
 \] $$
 We define
 $latex \[
 \begin{array}{rcl}
-b_0 & = & w_1 / 2 - ( w_1 - w_0 ) / 3 
-= ( w_1 + 2 w_0 ) / 6
+b_1 & = & w_2 / 2 - ( w_2 - w_1 ) / 3 
+= ( w_2 + 2 w_1 ) / 6
 \\
-b_1 & = & w_0 / 2 + ( w_1 - w_0 ) / 3 
-= ( w_0 + 2 w_1 ) / 6
+b_2 & = & w_1 / 2 + ( w_2 - w_1 ) / 3 
+= ( w_1 + 2 w_2 ) / 6
 \end{array}
 \] $$
 Using the definition of $latex v(s)$$ to replace 
-$latex v( r_0 )$$ and $latex v( r_1 )$$, we have
+$latex v( r_1 )$$ and $latex v( r_2 )$$, we have
 $latex \[
-d^{-1} I( v_0, v_1) =  
-b_0 \frac{( q_1 - r_0 ) v_0 + ( r_0 - q_0 ) v_1 }{ q_1 - q_0 }
+d^{-1} I( v_1, v_2) =  
+b_1 \frac{( q_2 - r_1 ) v_1 + ( r_1 - q_1 ) v_2 }{ q_2 - q_1 }
 +
-b_1 \frac{( q_1 - r_1 ) v_0 + ( r_1 - q_0 ) v_1 }{ q_1 - q_0 }
+b_2 \frac{( q_2 - r_2 ) v_1 + ( r_2 - q_1 ) v_2 }{ q_2 - q_1 }
 \] $$
 Finally, making the definitions
 $latex \[
 \begin{array}{rcl}
-c_0 & = &
-[  b_0 ( q_1 - r_0 ) + b_1 ( q_1 - r_1 ) ] \; d / ( q_1 - q_0 )
-\\
 c_1 & = &
-[ b_0 ( r_0 - q_0 ) + b_1 ( r_1 - q_0 ) ] \; d / ( q_1 - q_0 )
+[  b_1 ( q_2 - r_1 ) + b_2 ( q_2 - r_2 ) ] \; d / ( q_2 - q_1 )
+\\
+c_2 & = &
+[ b_1 ( r_1 - q_1 ) + b_2 ( r_2 - q_1 ) ] \; d / ( q_2 - q_1 )
 \end{array}
 \] $$
 We obtain the desired conclusion
 $latex \[
-I( v_0 , v_1 ) = c_0 v_0 + c_1 v_1
+I( v_1 , v_2 ) = c_1 v_1 + c_2 v_2
 \] $$
 $end
 ------------------------------------------------------------------------------
