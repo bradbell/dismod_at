b0aaa0d6b90514b243dae97c56a67412915d0f7c
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-07-29 20:36:35 +0000

    variable_vec is under construction.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@10213 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- devel/model/variable_vec.cpp
+++ devel/model/variable_vec.cpp
@@ -16,60 +16,57 @@ namespace dismod_at { // BEGIN DISMOD_AT_NAMESPACE
 
 template <class Float>
 variable_vec<Float>::variable_vec(
-	const CppAD::vector<run_struct>&     run_table   ,
-	const CppAD::vector<node_struct>&    node_table  ,
-	const CppAD::vector<data_struct>&    data_table
+	const CppAD::vector<run_struct>&     run_table      ,
+	const CppAD::vector<node_struct>&    node_table     ,
+	const CppAD::vector<data_struct>&    data_table     ,
+	const CppAD::vector<smooth_struct>&  smooth_table   ,
+	const CppAD::vector<mulcov_struct>&  mulcov_table
 )
 {	assert( run_table.size() == 1 );
 
 	// parent_node_id_
-	parent_node_id_ == run_table[0].parent_node;
+	size_t parent_node_id = run_table[0].parent_node;
 
 	// child_node_id_
-	assert( child_node_id_.size() == 0 );
-	for(size_t node_id = 0; node_id < node_table.size(); node_id++)
-	{	if( node_table[node_id].parent == parent_node_id_ )
-			child_node_id_.push_back(node_id);
-	}
-	if( child_node_id_.size() < 2 )
-	{	if( child_node_id_.size() == 1 )
-			child_node_id_[0] = parent_node_id_;
-		else
-			child_node_id_.push_back(parent_node_id_);
+	assert( node_id_.size() == 0 );
+	node_id_.push_back( parent_node_id );
+	for(size_t i = 0; i < node_table.size(); i++)
+	{	if( node_table[i].parent == parent_node_id )
+			node_id_.push_back(i);
 	}
 
 	// data_id2child_index_
-	data_id2child_index_.resize( data_table.size() );
+	data_id2node_index_.resize( data_table.size() );
 	for(size_t data_id = 0; data_id < data_table.size(); data_id++)
 	{	size_t node_id = data_table[data_id].node_id;
-		bool   more    = true;
 		size_t j       = 0;
+		bool   more    = true;
 		while( more )
-		{	while( more )
-			{	more = node_id != child_node_id_[j];
-				if( more )
-				{	j++;
-					if( j >= child_node_id_.size() )
-						more = false;
-					else
-						more = node_id != child_node_id_[j];
-				}
+		{	bool found = false;
+			while( (! found) &&  j < node_id_.size() )
+			{	found = node_id == node_id_[j];
+				if( ! found )
+					j++;
 			}
-			if( node_id != parent_node_id_ && j >= child_node_id_.size() )
+			if( found )
+				more = false;
+			else 
 			{	node_id = node_table[node_id].parent;
 				more    = node_id != size_t(-1);
 				j       = 0;
 			}
 		}
-		if( j < child_node_id_.size() )
-			data_id2child_index_[data_id] = j;
-		else if ( node_id == parent_node_id_ )
-			data_id2child_index_[data_id] = child_node_id_.size();
+		if( node_id == size_t(-1) )
+			data_id2node_index_[data_id] = size_t(-1);
 		else
-		{	assert( node_id == size_t(-1) );
-			data_id2child_index_[data_id] = child_node_id_.size() + 1;
-		}
+			data_id2node_index_[data_id] = j;
 	}
-};
+
+	// smooth_table_
+	smooth_table_ = smooth_table;
+
+	// rate_mean_mulcov_covariate_id_ 
+	// rate_mean_mulcov_smooth_id_
+}
 
 } // END DISMOD_AT_NAMESPACE
--- dismod_at/include/variable_vec.hpp
+++ dismod_at/include/variable_vec.hpp
@@ -16,46 +16,45 @@ see http://www.gnu.org/licenses/agpl.txt
 # include "get_node_table.hpp"
 # include "get_data_table.hpp"
 # include "get_smooth_table.hpp"
+# include "get_mulcov_table.hpp"
 # include "smooth_info.hpp"
 
 namespace dismod_at { // BEGIN DISMOD_AT_NAMESPACE
 
 template <class Float>
 class variable_vec {
+	typedef struct { size_t covariate_id; size_t smooth_id; } mulcov_pair;
 private:
-	// The parent and child nodes. If there is less than two children for this
-	// parent, child_node_id_ will have size one and its element will be
-	// set to the parent node id (set by constructor only)
-	size_t parent_node_id_;
-	CppAD::vector<size_t> child_node_id_;
+	// The parent node is node_id_[0]. The child nodes are node_id_[i] for
+	// i = 1 , ... , node_id_.size() - 1  (set by constructor only)
+	CppAD::vector<size_t> node_id_;
 
-	// Mapping from the data_id to the child node index, so we know which
-	// rates to use for each data point. Note that child_node_id.size()
-	// is used for parent node data and child_node_id.size() + 1 is used
-	// for nodes that are not below the parent.  (set by constructor only)
-	CppAD::vector<size_t> data_id2child_index_;
+	// Mapping from the data_id to the node_id_ index, so we know which
+	// rates to use for each data point. Note that node_id_[0] is the 
+	// parent node and the other nodes are child nodes. The value
+	// size_t(-1) is used for nodes that are not below the parent.  
+	// (set by constructor only)
+	CppAD::vector<size_t> data_id2node_index_;
 
 	// The smoothing standard deviation multipliers come frist in vec_
-	// and there are three per smoothing.  (set by constructor only)
-	CppAD::vector<size_t> mulstd_like_id_; 
+	// and there are three per smoothing; i.e., mulstd_value, mulstd_dage,
+	// and mulstd_dtime.  (set by constructor only)
+	CppAD::vector<smooth_struct> smooth_table_; 
 
 	// The rate_mean covariate multipliers come next in vec_ and
 	// each rate has its own vector of these multipliers.  
 	// (set by constructor only)
-	CppAD::vector< CppAD::vector<size_t> > rate_mean_mulcov_covariate_id_;
-	CppAD::vector< CppAD::vector<size_t> > rate_mean_mulcov_smooth_id_;
+	CppAD::vector< CppAD::vector<mulcov_pair> > rate_mean_mulcov_;
 
 	// The meas_mean covariate multipliers come next in vec_ and
 	// each integrand has its own vector of these multipliers.  
 	// (set by constructor only)
-	CppAD::vector< CppAD::vector<size_t> > meas_mean_mulcov_covariate_id_;
-	CppAD::vector< CppAD::vector<size_t> > meas_mean_mulcov_smooth_id_;
+	CppAD::vector< CppAD::vector<mulcov_pair> > meas_mean_mulcov_;
 
 	// The meas_std covariate multipliers come next in vec_ and
 	// each integrand has its own vector of these multipliers.  
 	// (set by constructor only)
-	CppAD::vector< CppAD::vector<size_t> > meas_std_mulcov_covariate_id_;
-	CppAD::vector< CppAD::vector<size_t> > meas_std_mulcov_smooth_id_;
+	CppAD::vector< CppAD::vector<mulcov_pair> > meas_std_mulcov_;
 
 	// The rate values for the child nodes come next in vec_. Note that
 	// each of the child nodes has the same smoothing. Also note
@@ -67,9 +66,11 @@ private:
 	CppAD::vector<Float> vec_;
 public:
 	variable_vec(
-		const CppAD::vector<run_struct>&     run_table   ,
-		const CppAD::vector<node_struct>&    node_table  ,
-		const CppAD::vector<data_struct>&    data_table
+		const CppAD::vector<run_struct>&     run_table      ,
+		const CppAD::vector<node_struct>&    node_table     ,
+		const CppAD::vector<data_struct>&    data_table     ,
+		const CppAD::vector<smooth_struct>&  smooth_table   ,
+		const CppAD::vector<mulcov_struct>&  mulcov_table
 	);
 };
 
