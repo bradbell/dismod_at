ecc1c651ab63d3627359d255161dc0dab8a58f97
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-07-29 14:22:01 +0000

    variable_vec.hpp: Under construction.
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@10211 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- dismod_at/include/variable_vec.hpp
+++ dismod_at/include/variable_vec.hpp
@@ -19,22 +19,48 @@ namespace dismod_at { // BEGIN DISMOD_AT_NAMESPACE
 
 template <class Float>
 class variable_vec {
-	struct subvector {
-		// offset in vec_ where this subvector starts
-		size_t offset_;
-		// smoothing used for each subvector
-		const smooth_info& sg_;
-	};
 private:
-	// Informaiton for each subvector
-	CppAD::vector<subvector> subvec_; 
+	// Number of child nodes
+	// (set by constructor only)
+	size_t n_child_;
 
-	// value for all the variables
+	// Mapping from the data_id to the child node index, so we know which
+	// rates to use for each data point. Note that n_child_ is used for
+	// parent node.  (set by constructor only)
+	CppAD::vector<size_t> data_id2child_index_;
+
+	// The smoothing standard deviation multipliers come frist in vec_
+	// and there are three per smoothing.  (set by constructor only)
+	CppAD::vector<size_t> mulstd_like_id_; 
+
+	// The rate_mean covariate multipliers come next in vec_ and
+	// each rate has its own vector of these multipliers.  
+	// (set by constructor only)
+	CppAD::vector< CppAD::vector<size_t> > rate_mean_mulcov_covariate_id_;
+	CppAD::vector< CppAD::vector<size_t> > rate_mean_mulcov_smooth_id_;
+
+	// The meas_mean covariate multipliers come next in vec_ and
+	// each integrand has its own vector of these multipliers.  
+	// (set by constructor only)
+	CppAD::vector< CppAD::vector<size_t> > meas_mean_mulcov_covariate_id_;
+	CppAD::vector< CppAD::vector<size_t> > meas_mean_mulcov_smooth_id_;
+
+	// The meas_std covariate multipliers come next in vec_ and
+	// each integrand has its own vector of these multipliers.  
+	// (set by constructor only)
+	CppAD::vector< CppAD::vector<size_t> > meas_std_mulcov_covariate_id_;
+	CppAD::vector< CppAD::vector<size_t> > meas_std_mulcov_smooth_id_;
+
+	// The rate values for the child nodes come next in vec_. Note that
+	// each of the child nodes has the same smoothing. Also note
+	// that the parent node value is the average of the child node values.
+	// (set by constructor only)
+	CppAD::vector<size_t> rate_smooth_id_;
+
+	// value of all the variables
 	CppAD::vector<Float> vec_;
 public:
-	variable_vec(
-		const CppAD::vector<smooth_info>& sg
-	);
+	variable_vec(void);
 };
 
 } // END DISMOD_AT_NAMESPACE:
