050a707e4a27cfb9aeee2952d8db04cf47810fb3
bradbell <bradbell@d6b170e6-0c5d-4075-bd8e-7951fab92de2>
2014-08-03 23:10:11 +0000

    get_run_table: only include necessary files
    git-svn-id: https://svn.ihme.washington.edu/svn/IHME/dismod_at/trunk@10365 d6b170e6-0c5d-4075-bd8e-7951fab92de2

--- devel/table/get_run_table.cpp
+++ devel/table/get_run_table.cpp
@@ -78,7 +78,9 @@ $end
 -----------------------------------------------------------------------------
 */
 
-# include <dismod_at/dismod_at.hpp>
+# include <dismod_at/include/get_run_table.hpp>
+# include <dismod_at/include/get_table_column.hpp>
+# include <dismod_at/include/check_table_id.hpp>
 
 namespace dismod_at { // BEGIN DISMOD_AT_NAMESPACE
 
