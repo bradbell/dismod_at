77ca592ece54af5a96f905963e412a68eeeca8b9
Brad Bell <bradbell@seanet.com>
2015-03-12 06:16:58 -0700

    bilinear_interp.hpp: fix indentation (white space).

--- dismod_at/include/bilinear_interp.hpp
+++ dismod_at/include/bilinear_interp.hpp
@@ -4,7 +4,7 @@ dismod_at: Estimating Disease Rate Estimation as Functions of Age and Time
           Copyright (C) 2014-14 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
-This program is distributed under the terms of the 
+This program is distributed under the terms of the
 	     GNU Affero General Public License version 3.0 or later
 see http://www.gnu.org/licenses/agpl.txt
 -------------------------------------------------------------------------- */
@@ -13,16 +13,16 @@ see http://www.gnu.org/licenses/agpl.txt
 
 # include <cppad/vector.hpp>
 
-	namespace dismod_at {
+namespace dismod_at {
 		double bilinear_interp(
-		double                         x          ,
-		double                         y          ,
-		const CppAD::vector<double>&   x_grid     ,
-		const CppAD::vector<double>&   y_grid     ,
-		const CppAD::vector<double>&   z_grid     ,
-		size_t&                        i          ,
-		size_t&                        j
-	);
+			double                         x          ,
+			double                         y          ,
+			const CppAD::vector<double>&   x_grid     ,
+			const CppAD::vector<double>&   y_grid     ,
+			const CppAD::vector<double>&   z_grid     ,
+			size_t&                        i          ,
+			size_t&                        j
+		);
 }
 
 # endif
