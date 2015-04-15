fe9d3edf57c063ef284bf0d18bbaebe2bf1e5021
bradbell <bradbell@seanet.com>
2015-04-15 15:30:04 -0700

    ipopt_fixed.cpp: remove indent from emtpy namespace area.

--- devel/approx_mixed/ipopt_fixed.cpp
+++ devel/approx_mixed/ipopt_fixed.cpp
@@ -22,110 +22,110 @@ $codep */
 # include <coin/IpIpoptApplication.hpp>
 # include <coin/IpTNLP.hpp>
 # include <cassert>
-namespace {
-	// Ipopt types used by this file
-	typedef Ipopt::Number  Number;
-	typedef Ipopt::Index   Index;
-	typedef Ipopt::TNLP::IndexStyleEnum IndexStyleEnum;
+namespace { // BEGIN EMPTY NAMESPACE
+// Ipopt types used by this file
+typedef Ipopt::Number  Number;
+typedef Ipopt::Index   Index;
+typedef Ipopt::TNLP::IndexStyleEnum IndexStyleEnum;
+//
+// ipopt_fixed_nlp
+class ipopt_fixed_nlp : public Ipopt::TNLP
+{
+public:
+	// did finalize_solution agree that the solution had converged
+	bool finalize_solution_ok_;
 	//
-	// ipopt_fixed_nlp
-	class ipopt_fixed_nlp : public Ipopt::TNLP
-	{
-	public:
-		// did finalize_solution agree that the solution had converged
-		bool finalize_solution_ok_;
-		//
-		// default constructor
-		ipopt_fixed_nlp(void);
-		//
-		// default destructor
-		virtual ~ipopt_fixed_nlp(void);
-		//
-		virtual bool get_nlp_info(
-			Index&          n            ,
-			Index&          m            ,
-			Index&          nnz_jac_g    ,
-			Index&          nnz_h_lag    ,
-			IndexStyleEnum& index_style
-		);
-		virtual bool get_bounds_info(
-				Index       n        ,
-				Number*     x_l      ,
-				Number*     x_u      ,
-				Index       m        ,
-				Number*     g_l      ,
-				Number*     g_u
-		);
-		virtual bool get_starting_point(
-			Index           n            ,
-			bool            init_x       ,
-			Number*         x            ,
-			bool            init_z       ,
-			Number*         z_L          ,
-			Number*         z_U          ,
-			Index           m            ,
-			bool            init_lambda  ,
-			Number*         lambda
-		);
-		virtual bool eval_f(
-			Index           n        ,
-			const Number*   x        ,
-			bool            new_x    ,
-			Number&         obj_value
-		);
-		virtual bool eval_grad_f(
-			Index           n        ,
-			const Number*   x        ,
-			bool            new_x    ,
-			Number*         grad_f
-		);
-		virtual bool eval_g(
-			Index           n        ,
-			const Number*   x        ,
-			bool            new_x    ,
-			Index           m        ,
-			Number*         g
-		);
-		virtual bool eval_jac_g(
-			Index           n        ,
-			const Number*   x        ,
-			bool            new_x    ,
-			Index           m        ,
-			Index           nele_jac ,
-			Index*          iRow     ,
-			Index*          jCol     ,
-			Number*         values
-		);
-		virtual bool eval_h(
-			Index         n              ,
-			const Number* x              ,
-			bool          new_x          ,
-			Number        obj_factor     ,
-			Index         m              ,
-			const Number* lambda         ,
-			bool          new_lambda     ,
-			Index         nele_hess      ,
-			Index*        iRow           ,
-			Index*        jCol           ,
-			Number*       values
-		);
-		virtual void finalize_solution(
-			Ipopt::SolverReturn               status    ,
-			Index                             n         ,
-			const Number*                     x         ,
-			const Number*                     z_L       ,
-			const Number*                     z_U       ,
-			Index                             m         ,
-			const Number*                     g         ,
-			const Number*                     lambda    ,
-			Number                            obj_value ,
-			const Ipopt::IpoptData*           ip_data   ,
-			Ipopt::IpoptCalculatedQuantities* ip_cq
-		);
+	// default constructor
+	ipopt_fixed_nlp(void);
+	//
+	// default destructor
+	virtual ~ipopt_fixed_nlp(void);
+	//
+	virtual bool get_nlp_info(
+		Index&          n            ,
+		Index&          m            ,
+		Index&          nnz_jac_g    ,
+		Index&          nnz_h_lag    ,
+		IndexStyleEnum& index_style
+	);
+	virtual bool get_bounds_info(
+			Index       n        ,
+			Number*     x_l      ,
+			Number*     x_u      ,
+			Index       m        ,
+			Number*     g_l      ,
+			Number*     g_u
+	);
+	virtual bool get_starting_point(
+		Index           n            ,
+		bool            init_x       ,
+		Number*         x            ,
+		bool            init_z       ,
+		Number*         z_L          ,
+		Number*         z_U          ,
+		Index           m            ,
+		bool            init_lambda  ,
+		Number*         lambda
+	);
+	virtual bool eval_f(
+		Index           n        ,
+		const Number*   x        ,
+		bool            new_x    ,
+		Number&         obj_value
+	);
+	virtual bool eval_grad_f(
+		Index           n        ,
+		const Number*   x        ,
+		bool            new_x    ,
+		Number*         grad_f
+	);
+	virtual bool eval_g(
+		Index           n        ,
+		const Number*   x        ,
+		bool            new_x    ,
+		Index           m        ,
+		Number*         g
+	);
+	virtual bool eval_jac_g(
+		Index           n        ,
+		const Number*   x        ,
+		bool            new_x    ,
+		Index           m        ,
+		Index           nele_jac ,
+		Index*          iRow     ,
+		Index*          jCol     ,
+		Number*         values
+	);
+	virtual bool eval_h(
+		Index         n              ,
+		const Number* x              ,
+		bool          new_x          ,
+		Number        obj_factor     ,
+		Index         m              ,
+		const Number* lambda         ,
+		bool          new_lambda     ,
+		Index         nele_hess      ,
+		Index*        iRow           ,
+		Index*        jCol           ,
+		Number*       values
+	);
+	virtual void finalize_solution(
+		Ipopt::SolverReturn               status    ,
+		Index                             n         ,
+		const Number*                     x         ,
+		const Number*                     z_L       ,
+		const Number*                     z_U       ,
+		Index                             m         ,
+		const Number*                     g         ,
+		const Number*                     lambda    ,
+		Number                            obj_value ,
+		const Ipopt::IpoptData*           ip_data   ,
+		Ipopt::IpoptCalculatedQuantities* ip_cq
+	);
 
 
-	};
-}
+};
+} // END_EMPTY_NAMESPACE
 /* $$
 $$ $comment end of nospell$$
 $end
