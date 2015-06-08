92b0a880c47b84052c6fc172c57ddeb4a3fd3717
Brad Bell <bradbell@seanet.com>
2015-06-08 13:06:24 -0700

    Prepare to split up database.py into separate utilities.

--- python/dismod_at/__init__.py
+++ python/dismod_at/__init__.py
@@ -1,11 +1,20 @@
 # $Id$
 #  --------------------------------------------------------------------------
 # dismod_at: Estimating Disease Rates as Functions of Age and Time
-#           Copyright (C) 2014-14 University of Washington
+#           Copyright (C) 2014-15 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
 # 	     GNU Affero General Public License version 3.0 or later
 # see http://www.gnu.org/licenses/agpl.txt
 # -------------------------------------------------------------------------- */
+# $begin python$$ $newlinech #$$
+#
+# $section Python Utilities$$
+#
+# $childtable%python/dismod_at/database.py
+# %$$
+#
+# $end
+# -----------------------------------------------------------------------------
 from database import *
