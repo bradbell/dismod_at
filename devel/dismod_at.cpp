3661dc11183b207873c013567ab2281a91360efc
Brad Bell <bradbell@seanet.com>
2015-06-23 13:25:06 -0700

    Change dismod_at fit command to overwrite its results tables.

--- devel/dismod_at.cpp
+++ devel/dismod_at.cpp
@@ -9,41 +9,70 @@ This program is distributed under the terms of the
 see http://www.gnu.org/licenses/agpl.txt
 -------------------------------------------------------------------------- */
 /*
-$begin dismod_at_exe$$
-$escape $$
+$begin fit_command$$
 $spell
+	tol
+	arg
 	Dismod
 	Ipopt
 	num_iter
 $$
 
-$section The Dismod Age Time Executable$$
+$section The Fit Command$$
 
 $head Syntax$$
-$codei%dismod_at %command% %file_name% %fit_id%$$
-
-$head command$$
-The only valid value (so far) for $icode command$$
-is $code fit$$.
-Other commands, such as sample from the
-posterior distribution, are planned for the future.
+$codei%dismod_at fit %file_name% %parent_node_id% \
+	%n_age_ode% %n_time_ode% %ode_step_size% %tolerance% %max_num_iter%$$
 
 $head file_name$$
 Is an
 $href%http://www.sqlite.org/sqlite/%$$ data base containing the
-$code dismod_at$$ $cref input$$ and $code output$$ tables.
-The input tables are not modified and result are written to the
-output tables.
+$code dismod_at$$ $cref input$$ tables which are not modified.
 
-$head fit_id$$
-This is a non-negative integer and specifies which row of the
-$cref/fit_table/fit_table/fit_id/$$ will be used for this fit.
+$subhead fit_arg_table$$
+A new $cref fit_arg_table$$ table is created with the arguments to
+this fit command; i.e., $icode file_name$$, ... , $icode max_num_iter$$.
+
+$subhead fit_result_table$$
+A new $cref fit_result_table$$ is created with the results of this
+fit command.
+
+$head parent_node_id$$
+This is a non-negative integer (greater than or equal zero)
+that specifies the parent $cref/node_id/node_table/node_id/$$.
+
+$head n_age_ode$$
+This is a positive integer (greater than zero)
+specifying the number of points in the
+$cref/ode age grid/glossary/Ode Grid/Age, a_i/$$.
+
+$head n_time_ode$$
+This is a positive integer
+specifying the number of points in the
+$cref/ode time grid/glossary/Ode Grid/Time, t_j/$$.
+
+$head ode_step_size$$
+This is a positive floating point number
+that specifies numerical ode step size.
+The step size is the same in age and time because it is along cohort lines.
+
+$head tolerance$$
+This is a positive floating point number
+and specifies the $href%http://www.coin-or.org/Ipopt/documentation/%Ipopt%$$
+desired relative convergence tolerance $code tol$$.
+
+$head max_num_iter$$
+This is a positive integer
+and specifies the
+$href%http://www.coin-or.org/Ipopt/documentation/%Ipopt%$$
+maximum number of iterations $code max_iter$$.
 
 $end
 */
 # include <string>
 # include <iostream>
 # include <cppad/vector.hpp>
+# include <dismod_at/configure.hpp>
 # include <dismod_at/open_connection.hpp>
 # include <dismod_at/get_db_input.hpp>
 # include <dismod_at/fit_model.hpp>
@@ -51,7 +80,7 @@ $end
 # include <dismod_at/put_table_row.hpp>
 # include <dismod_at/to_string.hpp>
 # include <dismod_at/get_column_max.hpp>
-# include <dismod_at/configure.hpp>
+# include <dismod_at/exec_sql_cmd.hpp>
 
 int main(int n_arg, const char** argv)
 {	// ---------------- using statements ----------------------------------
@@ -59,49 +88,54 @@ int main(int n_arg, const char** argv)
 	using std::endl;
 	using CppAD::vector;
 	using std::string;
-
+	// -----------------------------------------------------------------------
+	// fit command line argument names
+	const char* fit_arg_name[] = {
+		"file_name",
+		"parent_node_id",
+		"n_age_ode",
+		"n_time_ode",
+		"ode_step_size",
+		"tolerance",
+		"max_num_iter"
+	};
+	size_t n_fit_arg = sizeof(fit_arg_name) / sizeof(fit_arg_name[0]);
+	//
 	// ---------------- command line arguments ---------------------------
-	const char* usage = "dismod_at command file_name fit_id";
-	if( n_arg != 4 )
-	{	cerr << "dismod_at-" << DISMOD_AT_VERSION << endl
-		<< "\t" << usage << endl <<
-		"Expected 3 command line argument but found " << n_arg - 1 << endl;
+	string usage = "dismod_at-";
+	usage       += DISMOD_AT_VERSION;
+	usage       += "\ndismod_at fit";
+	for(size_t i = 0; i < n_fit_arg; i++)
+	{	usage += " ";
+		usage += fit_arg_name[i];
+	}
+	if( size_t(n_arg) != n_fit_arg + 2 )
+	{	cerr << usage << endl;
 		exit(1);
 	}
 	size_t i_arg = 0;
 	const string command_arg       = argv[++i_arg];
-	const string file_name_arg     = argv[++i_arg];
-	const string fit_id_arg        = argv[++i_arg];
-	// ------------------------------------------------------------------
 	if( command_arg != "fit" )
-	{	cerr << usage << endl <<
-		"Only the 'fit' command is supported (so far)" << endl;
+	{	cerr << usage << endl;
 		exit(1);
 	}
+	const string file_name_arg      = argv[++i_arg];
+	const string parent_node_id_arg = argv[++i_arg];
+	const string n_age_ode_arg      = argv[++i_arg];
+	const string n_time_ode_arg     = argv[++i_arg];
+	const string ode_step_size_arg  = argv[++i_arg];
+	const string tolerance_arg      = argv[++i_arg];
+	const string max_num_iter_arg   = argv[++i_arg];
 	// --------------- get the input tables -----------------------------
 	bool new_file = false;
 	sqlite3* db   = dismod_at::open_connection(file_name_arg, new_file);
 	dismod_at::db_input_struct db_input;
 	get_db_input(db, db_input);
-	// ----------------- fit_id -----------------------------------------
-	size_t fit_id = static_cast<size_t>( std::atoi( fit_id_arg.c_str() ) );
-	if( fit_id >= db_input.fit_table.size() )
-	{	cerr << usage << endl <<
-		"Invalid value for fit_id (not in fit table)" << endl;
-		exit(1);
-	}
 	// ---------------------------------------------------------------------
-	size_t parent_node_id = size_t(db_input.fit_table[fit_id].parent_node_id);
-	size_t n_age_ode      = size_t(db_input.fit_table[fit_id].n_age_ode);
-	size_t n_time_ode     = size_t(db_input.fit_table[fit_id].n_time_ode);
-	double ode_step_size  = db_input.fit_table[fit_id].ode_step_size;
-	// 2DO: remove need to convert these two strings to values in run_fit
-	string tolerance_str  = dismod_at::to_string(
-		db_input.fit_table[fit_id].tolerance
-	);
-	string max_num_iter_str = dismod_at::to_string(
-		db_input.fit_table[fit_id].max_num_iter
-	);
+	size_t parent_node_id = std::atoi( parent_node_id_arg.c_str() );
+	size_t n_age_ode      = std::atoi( n_age_ode_arg.c_str() );
+	size_t n_time_ode     = std::atoi( n_time_ode_arg.c_str() );
+	double ode_step_size  = std::atof( ode_step_size_arg.c_str() );
 	dismod_at::child_info child_object(
 		parent_node_id ,
 		db_input.node_table ,
@@ -173,24 +207,50 @@ int main(int n_arg, const char** argv)
 		data_object          ,
 		prior_object
 	);
-	fit_object.run_fit(tolerance_str, max_num_iter_str);
+	fit_object.run_fit(tolerance_arg, max_num_iter_arg);
 	vector<double> solution = fit_object.get_solution();
-	// ----------------- variable_table ----------------------------------
-	CppAD::vector<string> col_name_vec(4), row_val_vec(4);
-	string table_name = "variable";
-	col_name_vec[0]   = "fit_id";
-	col_name_vec[1]   = "sample";
-	col_name_vec[2]   = "offset";
-	col_name_vec[3]   = "value";
+	// ----------------- fit_arg_table ----------------------------------
+	const char* sql_cmd;
+	CppAD::vector<string> col_name_vec, row_val_vec;
+	string table_name;
 	//
-	// maximum likelihood sample
-	size_t sample = 0;
+	sql_cmd = "drop table if exists fit_arg";
+	dismod_at::exec_sql_cmd(db, sql_cmd);
+	sql_cmd = "create table fit_arg("
+		"fit_arg_id integer primary key, fit_arg_name text, fit_arg_value text"
+	")";
+	dismod_at::exec_sql_cmd(db, sql_cmd);
+	table_name = "fit_arg";
 	//
-	row_val_vec[0]  = dismod_at::to_string( fit_id );
-	row_val_vec[1]  = dismod_at::to_string( sample );
-	for(size_t offset = 0; offset < solution.size(); offset++)
-	{	row_val_vec[2] = dismod_at::to_string( offset );
-		row_val_vec[3] = dismod_at::to_string( solution[offset] );
+	col_name_vec.resize(3);
+	col_name_vec[0]   = "fit_arg_id";
+	col_name_vec[1]   = "fit_arg_name";
+	col_name_vec[2]   = "fit_arg_value";
+	//
+	row_val_vec.resize(3);
+	for(size_t id = 0; id < n_fit_arg; id++)
+	{	row_val_vec[0] = dismod_at::to_string(id);
+		row_val_vec[1] = fit_arg_name[id];
+		row_val_vec[2] = argv[2 + id];
+		dismod_at::put_table_row(db, table_name, col_name_vec, row_val_vec);
+	}
+	// ----------------- fit_result_table ----------------------------------
+	sql_cmd = "drop table if exists fit_result";
+	dismod_at::exec_sql_cmd(db, sql_cmd);
+	sql_cmd = "create table fit_result("
+		"fit_result_id integer primary key, fit_result_value real"
+	")";
+	dismod_at::exec_sql_cmd(db, sql_cmd);
+	table_name = "fit_result";
+	//
+	col_name_vec.resize(2);
+	col_name_vec[0]   = "fit_result_id";
+	col_name_vec[1]   = "fit_result_value";
+	//
+	row_val_vec.resize(2);
+	for(size_t id = 0; id < solution.size(); id++)
+	{	row_val_vec[0] = dismod_at::to_string( id );
+		row_val_vec[1] = dismod_at::to_string( solution[id] );
 		dismod_at::put_table_row(db, table_name, col_name_vec, row_val_vec);
 	}
 	// ---------------------------------------------------------------------
