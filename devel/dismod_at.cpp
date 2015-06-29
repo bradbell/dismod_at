bd319367ee169d0f0680a17d3841292fe496094d
Brad Bell <bradbell@seanet.com>
2015-06-29 16:54:24 -0700

    Add the simulate command (under construction).
    fit_arg_table.omh: fix name of last arg for fit command.

--- devel/dismod_at.cpp
+++ devel/dismod_at.cpp
@@ -30,17 +30,13 @@ $spell
 	Dismod
 	Ipopt
 	num_iter
-	mtspecific
-	mtall
-	mtstandard
 $$
 
 $section The Fit Command$$
 
 $head Syntax$$
-$codei%dismod_at fit %file_name% %parent_node_id% \
-	%n_age_ode% %n_time_ode% %ode_step_size% %tolerance% %max_num_iter%$$ \
-	[ rho_zero ] [chi_zero]
+$codei%dismod_at fit %file_name% %parent_node_id% %n_age_ode% %n_time_ode% \
+	%ode_step_size% %tolerance% %max_num_iter% %rate_info%$$
 
 $head file_name$$
 Is an
@@ -49,7 +45,7 @@ $code dismod_at$$ $cref input$$ tables which are not modified.
 
 $subhead fit_arg_table$$
 A new $cref fit_arg_table$$ table is created with the arguments to
-this fit command; i.e., $icode file_name$$, ... , $icode max_num_iter$$.
+this fit command; i.e., $icode file_name$$, ... , $icode rate_info$$.
 
 $subhead variable_table$$
 A new $cref variable_table$$ is created with the maximum likelihood
@@ -92,49 +88,13 @@ and specifies the
 $href%http://www.coin-or.org/Ipopt/documentation/%Ipopt%$$
 maximum number of iterations $code max_iter$$.
 
-$head rate_info$$
-This argument only matters when one of the following
-$cref/integrand names/integrand_table/integrand_name/$$ is used:
-$code prevalence$$,
-$code mtspecific$$,
-$code mtall$$,
-$code mtstandard$$.
-In this case it is necessary to solve the dismod_ode
-$cref/
-ordinary differential equation /avg_integrand/Ordinary Differential Equation
-/$$
-The possible values for this argument are listed below:
-
-$subhead chi_positive$$
-If $icode%rate_info% = "chi_positive"%$$,
-the $cref/parent smoothing/rate_table/parent_smooth_id/$$ for
-$icode chi$$ always has a lower limit greater than zero.
-
-$subhead iota_and_chi_zero$$
-If $icode%rate_info% = "iota_and_chi_zero"%$$,
-the $cref/parent smoothing/rate_table/parent_smooth_id/$$ for both
-$icode iota$$ and $icode chi$$ have lower and upper limit zero.
-In this case
-the $cref/parent smoothing/rate_table/parent_smooth_id/$$ for
-$icode rho$$ must always has a lower limit greater than zero.
-
-$subhead rho_and_chi_zero$$
-If $icode%rate_info% = "rho_and_chi_zero"%$$,
-the $cref/parent smoothing/rate_table/parent_smooth_id/$$ for both
-$icode rho$$ and $icode chi$$ have lower and upper limit zero.
-In this case
-the $cref/parent smoothing/rate_table/parent_smooth_id/$$ for
-$icode iota$$ must always has a lower limit greater than zero.
-
-$subhead iota_and_rho_zero$$
-If $icode%rate_info% = "iota_and_rho_zero"%$$,
-the $cref/parent smoothing/rate_table/parent_smooth_id/$$ for
-both $icode iota$$ and $icode rho$$ have lower and upper limit zero.
+$include devel/rate_info.omh$$
 
 $end
 */
 namespace { // BEGIN_EMPTY_NAMESPACE
 
+// ----------------------------------------------------------------------------
 void fit_command(
 	sqlite3*                                     db               ,
 	const dismod_at::pack_info&                  pack_object      ,
@@ -223,6 +183,77 @@ void fit_command(
 	}
 	return;
 }
+// ----------------------------------------------------------------------------
+/*
+$begin simulate_command$$
+
+$section The Simulate Command$$
+$spell
+	dismod
+	arg
+	std
+$$
+
+$head Under Construction$$
+This command is under construction and does not yet work.
+
+$head Syntax$$
+$codei%dismod_at simulate %file_name% %parent_node_id% \
+	%n_age_ode% %n_time_ode% %ode_step_size% %rate_info%$$
+
+$head file_name$$
+Is an
+$href%http://www.sqlite.org/sqlite/%$$ data base containing the
+$code dismod_at$$ $cref input$$ tables which are not modified.
+
+$subhead variable_table$$
+The data base must contains a $cref variable_table$$ that specifies the
+value of the model variables that is used to simulate the data.
+The $cref/variable_name/variable_table/variable_name/$$ column
+is not used.
+
+$subhead simulate_table$$
+A new $cref simulate_table$$ table is created with the arguments to
+this simulate command;
+i.e., $icode file_name$$, ... , $icode rate_info$$.
+
+$subhead data_table$$
+The measurement values for which
+$cref/node_id/data_table/node_id/$$ is $icode parent_node_id$$,
+or that is a descendent of $icode parent_node_id$$ are replaced by
+simulated values.
+
+$head parent_node_id$$
+This is a non-negative integer (greater than or equal zero)
+that specifies the parent $cref/node_id/node_table/node_id/$$
+for this simulation (see data_table heading above).
+
+$head n_age_ode$$
+This is a positive integer (greater than zero)
+specifying the number of points in the
+$cref/ode age grid/glossary/Ode Grid/Age, a_i/$$.
+
+$head n_time_ode$$
+This is a positive integer
+specifying the number of points in the
+$cref/ode time grid/glossary/Ode Grid/Time, t_j/$$.
+
+$head ode_step_size$$
+This is a positive floating point number
+that specifies numerical ode step size.
+The step size is the same in age and time because it is along cohort lines.
+
+$include devel/rate_info.omh$$
+
+$end
+*/
+void simulate_command(void)
+{	using std::cerr;
+	using std::endl;
+
+	cerr << "dismod_at simulate command not yet implemented" << endl;
+	return;
+}
 } // END_EMPTY_NAMESPACE
 
 int main(int n_arg, const char** argv)
@@ -244,23 +275,56 @@ int main(int n_arg, const char** argv)
 		"rate_info"
 	};
 	size_t n_fit_arg = sizeof(fit_arg_name) / sizeof(fit_arg_name[0]);
+	// -----------------------------------------------------------------------
+	// simulate command line argument names
+	const char* simulate_arg_name[] = {
+		"file_name",
+		"parent_node_id",
+		"n_age_ode",
+		"n_time_ode",
+		"ode_step_size",
+		"rate_info"
+	};
+	size_t n_simulate_arg =
+		sizeof(simulate_arg_name) / sizeof(simulate_arg_name[0]);
 	//
 	// ---------------- command line arguments ---------------------------
-	string usage = "dismod_at-";
-	usage       += DISMOD_AT_VERSION;
-	usage       += "\ndismod_at fit";
+	string program = "dismod_at-";
+	program       += DISMOD_AT_VERSION;
+	string fit_usage   = "dismod_at fit";
 	for(size_t i = 0; i < n_fit_arg; i++)
-	{	usage += " ";
-		usage += fit_arg_name[i];
+	{	fit_usage += " ";
+		fit_usage += fit_arg_name[i];
 	}
-	if( size_t(n_arg) != n_fit_arg + 2 )
-	{	cerr << usage << endl;
+	string simulate_usage   = "dismod_at simulate";
+	for(size_t i = 0; i < n_simulate_arg; i++)
+	{	simulate_usage += " ";
+		simulate_usage += simulate_arg_name[i];
+	}
+	if( n_arg < 2 )
+	{	cerr << program << endl;
+		cerr << fit_usage << endl;
+		cerr << simulate_usage << endl;
 		std::exit(1);
 	}
 	size_t i_arg = 0;
-	const string command_arg       = argv[++i_arg];
-	if( command_arg != "fit" )
-	{	cerr << usage << endl;
+	const string command_arg  = argv[++i_arg];
+	if( command_arg == "fit" )
+	{	if( size_t(n_arg) != n_fit_arg + 2 )
+		{	cerr << fit_usage << endl;
+			std::exit(1);
+		}
+	}
+	else if( command_arg == "simulate" )
+	{	if( size_t(n_arg) != n_simulate_arg + 2 )
+		{	cerr << simulate_usage << endl;
+			std::exit(1);
+		}
+	}
+	else
+	{	cerr << program << endl;
+		cerr << fit_usage << endl;
+		cerr << simulate_usage << endl;
 		std::exit(1);
 	}
 	const string file_name_arg      = argv[++i_arg];
@@ -268,8 +332,11 @@ int main(int n_arg, const char** argv)
 	const string n_age_ode_arg      = argv[++i_arg];
 	const string n_time_ode_arg     = argv[++i_arg];
 	const string ode_step_size_arg  = argv[++i_arg];
-	const string tolerance_arg      = argv[++i_arg];
-	const string max_num_iter_arg   = argv[++i_arg];
+	string tolerance_arg, max_num_iter_arg;
+	if( command_arg == "fit" )
+	{	tolerance_arg    = argv[++i_arg];
+		max_num_iter_arg = argv[++i_arg];
+	}
 	const string rate_info_arg      = argv[++i_arg];
 	// ------------- check rate_info_arg ------------------------------------
 	bool ok = rate_info_arg == "chi_positive";
@@ -277,8 +344,11 @@ int main(int n_arg, const char** argv)
 	ok     |= rate_info_arg == "rho_and_chi_zero";
 	ok     |= rate_info_arg == "iota_and_rho_zero";
 	if( ! ok )
-	{	cerr << usage << endl
-		<< "rate_info = " << rate_info_arg
+	{	if( command_arg == "fit" )
+			cerr << fit_usage << endl;
+		else
+			cerr << simulate_usage << endl;
+		cerr << "rate_info = " << rate_info_arg
 		<< " is not a valid choice" << endl;
 		std::exit(1);
 	}
@@ -363,7 +433,7 @@ int main(int n_arg, const char** argv)
 	);
 	data_object.set_eigen_ode2_case_number(rate_info_arg);
 	//
-	fit_command(
+	if( command_arg == "fit" ) fit_command(
 		db               ,
 		pack_object      ,
 		db_input         ,
@@ -378,6 +448,7 @@ int main(int n_arg, const char** argv)
 		parent_node_id   ,
 		child_object
 	);
+	if( command_arg == "simulate" ) simulate_command();
 	// ---------------------------------------------------------------------
 	sqlite3_close(db);
 	return 0;
