51c4fb852946fe45dcc9863fdf370df3c23bd91b
Brad Bell <bradbell@seanet.com>
2015-07-02 08:26:48 -0700

    Include Laplace distribution in example and test or random_sim.

--- example/devel/utility/sim_random_xam.cpp
+++ example/devel/utility/sim_random_xam.cpp
@@ -26,6 +26,7 @@ $end
 */
 // BEGIN C++
 # include <cmath>
+# include <cppad/vector.hpp>
 # include <dismod_at/get_density_table.hpp>
 # include <dismod_at/manage_gsl_rng.hpp>
 # include <dismod_at/sim_random.hpp>
@@ -33,31 +34,64 @@ $end
 bool sim_random_xam(void)
 {	bool ok = true;
 	//
-	size_t sample_size = 1000;
-	//
-	dismod_at::density_enum density;
-	double mu, delta, eta, probability, check;
-	size_t count;
 	//
 	// initialize random number generator using the clock
 	dismod_at::new_gsl_rng(0);
 	//
+	size_t sample_size = 1000;
+	double delta       = 1.0;
+	double mu          = 0.0;
+	// -------------------------------------------------------------------
 	// check Gausian
-	density = dismod_at::gaussian_enum;
-	mu      = 0.0;
-	delta   = 1.0;
-	eta     = 0.0; // not used (avoid warning)
-	count   = 0;
+	dismod_at::density_enum density = dismod_at::gaussian_enum;
+	size_t count   = 0;
+	double sum_z   = 0.0;
+	double sum_zsq = 0.0;
+	double eta     = 0.0; // not used (avoid warning)
 	for(size_t i = 0; i < sample_size; i++)
 	{	double z = dismod_at::sim_random(density, mu, delta, eta);
 		if( 0.5 <= z )
 			count ++;
+		sum_z   += z;
+		sum_zsq += z * z;
 	}
-	probability = double(count) / double(sample_size);
+	double samp_mean = sum_z / double(sample_size);
+	double samp_var  = sum_zsq / double(sample_size);
+	double samp_prob = double(count) / double(sample_size);
 	//
+	double check = 0.0;
+	ok   &= std::fabs(samp_mean) < 2.0 / std::sqrt( double(sample_size) );
+	check = delta;
+	ok   &= std::fabs(samp_var / check - 1.0) < 1e-1;
 	// https://en.wikipedia.org/wiki/Standard_normal_table
 	check = 0.5 - 0.19146;
-	ok   &= std::fabs( probability / check - 1.0 ) < 1e-1;
+	ok   &= std::fabs( samp_prob / check - 1.0 ) < 1e-1;
+	// -------------------------------------------------------------------
+	// check Laplace
+	density = dismod_at::laplace_enum;
+	count   = 0;
+	sum_z   = 0.0;
+	sum_zsq = 0.0;
+	eta     = 0.0; // not used (avoid warning)
+	for(size_t i = 0; i < sample_size; i++)
+	{	double z = dismod_at::sim_random(density, mu, delta, eta);
+		if( 0.5 <= z )
+			count ++;
+		sum_z   += z;
+		sum_zsq += z * z;
+	}
+	samp_mean = sum_z / double(sample_size);
+	samp_var  = sum_zsq / double(sample_size);
+	samp_prob = double(count) / double(sample_size);
+	//
+	check = 0.0;
+	ok   &= std::fabs(samp_mean) < 2.0 / std::sqrt( double(sample_size) );
+	check = delta;
+	ok   &= std::fabs(samp_var / check - 1.0) < 1e-1;
+	//
+	// integral from 0.5 to infinity of p(z) dz
+	check = exp( - std::sqrt(2.0) * 0.5 ) / 2.0;
+	ok   &= std::fabs( samp_prob / check - 1.0 ) < 1e-1;
 	//
 	// free random number generator
 	dismod_at::free_gsl_rng();
