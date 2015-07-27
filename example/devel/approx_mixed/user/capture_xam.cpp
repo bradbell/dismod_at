ed5386b67f02b654e8d9fc517bfe2348717067a7
Brad Bell <bradbell@seanet.com>
2015-07-27 06:03:04 -0700

    Move capture.cpp -> capture_xam.cpp

--- example/devel/approx_mixed/user/capture_xam.cpp
+++ example/devel/approx_mixed/user/capture_xam.cpp
@@ -9,7 +9,7 @@ This program is distributed under the terms of the
 see http://www.gnu.org/licenses/agpl.txt
 -------------------------------------------------------------------------- */
 /*
-$begin capture.cpp$$
+$begin capture_xam.cpp$$
 $spell
 	Biometrics
 	Royle
@@ -191,7 +191,7 @@ namespace {
 	}
 
 }
-bool capture(void)
+bool capture_xam(void)
 {	bool ok = true;
 
 	// simulation parameters
