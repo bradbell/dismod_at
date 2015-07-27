96c892f26d0e9423fb2a0be26acc3620cee0edb1
Brad Bell <bradbell@seanet.com>
2015-07-27 05:33:03 -0700

    Move public/approx_constraint_xam.cpp -> user/approx_constraint_xam.cpp

--- devel/approx_mixed/optimize_fixed.cpp
+++ devel/approx_mixed/optimize_fixed.cpp
@@ -156,7 +156,6 @@ Laplace approximation optimization only includes the case where
 the $cref/joint negative log-likelihood/approx_mixed_joint_like/$$ is smooth.
 
 $children%example/devel/approx_mixed/public/optimize_fixed_xam.cpp
-	%example/devel/approx_mixed/public/approx_constraint_xam.cpp
 	%include/dismod_at/ipopt_fixed.hpp
 %$$
 
