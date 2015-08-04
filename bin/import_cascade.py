4a6598b1c423cf05e65aa3f357fe53d1aaf0376c
Brad Bell <bradbell@seanet.com>
2015-08-04 08:40:49 -0700

    import_cascade.py: under construction.

--- bin/import_cascade.py
+++ bin/import_cascade.py
@@ -21,34 +21,47 @@
 import sys
 import os
 import csv
+sys.path.append( os.path.join( os.getcwd(), 'python' ) )
+import dismod_at
 # ---------------------------------------------------------------------------
-# Create cascade_path_dict (path to cascade input files)
+# cascade_dir:       directory name (not path) where cascade input files are
+# cascade_path_dict: path to cascade input files
 #
 if sys.argv[0] != 'bin/import_cascade.py' :
 	msg  = 'bin/import_cascasde.py: must be executed from its parent directory'
 	sys.exit(msg)
 #
-usage = 'bin/import_cascade.py cascade_dir'
+usage = 'bin/import_cascade.py cascade_path'
 n_arg = len(sys.argv)
 if n_arg != 2 :
 	sys.exit(usage)
 #
-cascade_dir = sys.argv[1]
-if not os.path.isdir( cascade_dir ) :
+cascade_path = sys.argv[1]
+if not os.path.isdir( cascade_path ) :
 	msg  = usage + '\n'
-	msg += 'import_cascade: ' + cascade_dir + ' is not a directory'
+	msg += 'import_cascade: ' + cascade_path + ' is not a directory'
 	sys.exit(msg)
+#
+cascade_dir       = os.path.basename(cascade_path)
 #
 cascade_name_list = [
 	'data', 'rate_prior', 'simple_prior', 'effect_prior', 'integrand', 'value'
 ]
 cascade_path_dict = dict()
 for name in cascade_name_list :
-	path = os.path.join(cascade_dir, name + '.csv')
+	path = os.path.join(cascade_path, name + '.csv')
 	if not os.path.isfile(path) :
 		msg = 'import_cascade: ' + path + ' is not a file'
 		sys.exit(msg)
 	cascade_path_dict[name] = path
+# ---------------------------------------------------------------------------
+# Create database_connection (database that is output by this program)
+if not os.path.isdir('build') :
+	print('mkdir build')
+	os.mkdir('build')
+new = True
+file_name        = os.path.join('build', cascade_dir + '.db')
+db_connection    = dismod_at.create_connection(file_name, new)
 # ---------------------------------------------------------------------------
 # Create cascade_data_dict (data for each cascade input file)
 #
