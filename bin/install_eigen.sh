921ed14c7eadd43ebd602b237d3b4b1ffbd74a29
Brad Bell <bradbell@seanet.com>
2015-08-18 09:13:36 -0700

    Fix minor problems with the install procedure (related to the eigen package).

--- bin/install_eigen.sh
+++ bin/install_eigen.sh
@@ -6,7 +6,7 @@
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
-# 	     GNU Affero General Public License version 3.0 or later
+#	     GNU Affero General Public License version 3.0 or later
 # see http://www.gnu.org/licenses/agpl.txt
 # ---------------------------------------------------------------------------
 # BEGIN USER_SETTINGS
@@ -53,7 +53,7 @@ echo_eval cd eigen-$version
 #
 echo 'patch eigen source code'
 sed -e 's|Scalar l_ii = 0|Scalar l_ii = Scalar(0)|' \
-	-i Eigen/src/SparseCore/TriangularSolver.h
+	-i.old Eigen/src/SparseCore/TriangularSolver.h
 #
 echo_eval mkdir build
 echo_eval cd build
