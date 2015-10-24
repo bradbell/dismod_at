fbc2f969ed60658b022a763703ca48c32b690008
Brad Bell <bradbell@seanet.com>
2015-10-24 12:48:56 -0700

    initialize.cpp: redo the return size_map.

--- devel/mixed_cppad/initialize.cpp
+++ devel/mixed_cppad/initialize.cpp
@@ -64,133 +64,77 @@ $cref/random effects/mixed_cppad/Random Effects, u/$$
 vector $latex u$$ at which certain $code CppAD::ADFun$$
 objects are recorded.
 
-$head newton_atom_$$
-The input value of the member variable
-$codei%
-	newton_step newton_atom_
-%$$
-must be the same as after its constructor; i.e.,
-no member functions had been called.
-Upon return, $code newton_atom_$$ can be used to compute
-the log of the determinant and the Newton step using
-$codei%
-	newton_atom_(%a1_theta_u_v%, %a1_logdet_step%)
-%$$
-be more specific, $icode%a1_logdet_step%[0]%$$ is the log of the determinant of
-$latex f_{uu}^{(2)} ( \theta , u ) $$ and the rest of the vector is the
-Newton step
-$latex \[
-	s = f_{uu}^{(2)} ( \theta , u )^{-1} v
-\] $$
-
 $head size_map$$
 The return value has prototype
 $codei%
 	std::map<std::string, size_t> %size_map%
 %$$
-It represent the size of certain aspects of the problem as follows:
+It represent the size of certain aspects of the problem.
+For each of $code mixed_cppad$$ member variables listed below
 $codei%
-
-%size_map%["fix_like"]
+	%size_map%[%member_variable%]
 %$$
-	number of variables in the $code ADFun<double>$$ version of
-	$cref/fix_like/mixed_cppad_fix_like/$$.
-$codei%
+is the corresponding size.
 
-%size_map%["a1_ran_like"]
-%$$
-number of variables in the $code ADFun<a1_double>$$ version of
-$cref/ran_like/mixed_cppad_ran_like/$$.
-$codei%
+$subhead hes_ran_row$$
+$icode%size_map%["hes_ran_row"]%$$ is the
+number of non-zero entries in hessian of
+random negative log-likelihood w.r.t random effects
+$latex f_{uu}^{(2)} ( \theta , u )$$.
 
-%size_map%["a0_ran_like"]
-%$$
+$subhead hes_cross_row$$
+$icode%size_map%["hes_cross_row"]%$$ is the
+number of non-zero cross entries in hessian of
+random negative log-likelihood w.r.t fixed and random effects
+$latex f_{u \theta}^{(2)} ( \theta , u )$$.
+
+$subhead a0_ran_like$$
+$icode%size_map%["a0_ran_like"]%$$ is the
 number of variables in the $code ADFun<double>$$ version of
 $cref/ran_like/mixed_cppad_ran_like/$$.
-$codei%
 
-%size_map%["hes_ran"]
-%$$
-number of non-zero entries in hessian of
-random negative log-likelihood
-$cref/f(theta, u)
-	/mixed_cppad_theory
-	/Random Negative Log-Likelihood, f(theta, u)
-/$$
-with respect to fixed effects $latex \theta$$.
-$codei%
+$subhead a1_ran_like$$
+$icode%size_map%["a1_ran_like"]%$$ is the
+number of variables in the $code ADFun<a1_double>$$ version of
+$cref/ran_like/mixed_cppad_ran_like/$$.
 
-%size_map%["newton_atom"]
-%$$
+$subhead hes_ran_0$$
+$icode%size_map%["hes_ran_0_"]%$$ is the
+number of variables in the $code ADFun<double>$$ object that
+computes the Hessian with respect to the random effects.
+
+$subhead newton_atom$$
+$icode%size_map%["newton_atom"]%$$ is the
 number of variables in the $code ADFun<double>$$
 object used to evaluate the newton step
 $latex \[
 	s = f_{uu}^{(2)} ( \theta , u )^{-1} v
 \] $$
 and the log of the determinant of the matrix being inverted.
-$codei%
 
-%size_map%["hes_fix"]
-%$$
-number of non-zero entries in hessian of fixed part of objective
-$cref/g(theta)
-	/mixed_cppad_theory
-	/Fixed Negative Log-Likelihood, g(theta)
-/$$
-with respect to the random effects $latex \theta$$.
-$codei%
-
-%size_map%["ran_obj_2"]
-%$$
+$subhead ran_obj_2$$
+$icode%size_map%["ran_obj_2"]%$$ is the
 number of variables in the $code ADFun<double>$$
-object used to evaluate Hessian, w.r.t. fixed effects, of the
-$cref/random part of objective
-	/mixed_cppad_theory
-	/Objective
-	/Random Part of Objective, r(theta)
-/$$.
-$codei%
+object used to evaluate Hessian, w.r.t. fixed effects, of the objective
+$latex r^{(2)} ( \theta )$$.
 
-%size_map%["num_bytes"]
-%$$
-number of bytes currently allocated by $code CppAD::thread_alloc$$
-for the current thread.
+$subhead fix_like$$
+$icode%size_map%["fix_like"]%$$ is the
+number of variables in the $code ADFun<double>$$ function
+that computes the fixed negative log likelihood $latex g( \theta )$$.
+
+$subhead constraint$$
+$icode%size_map%["constraint"]%$$ is the
+number of variables in the $code ADFun<double>$$ function
+that computes the general constraints for the fixed effects.
+
+$subhead num_bytes$$
+Is the number of bytes currently allocated by
+$code CppAD::thread_alloc$$ for the current thread.
 
 $head Example$$
 The file $cref mixed_derived_xam.cpp$$ contains an example
 of using $code initialize$$.
-
-$head Private Member Variables$$
-The following $code mixed_cppad$$ private member variables are
-$code CppAD::ADFun$$ functions that are assumed to be empty when
-$code initialize$$ is called; i.e.,
-the corresponding $code size_var()$$ is zero.
-They will contain the corresponding recordings when $code initialize$$ returns:
-$list number$$
-If $icode%n_random_ > 0%$$,
-for $icode%k% = 0%, ... ,% 4%$$,
-the member variable $codei%a%k%_ran_like_%$$ is the corresponding
-$cref/ran_like_/mixed_cppad_private/n_random_ > 0/ran_like_/$$.
-$lnext
-If $icode%n_random_ > 0%$$,
-the member variable
-$cref/hes_ran_/mixed_cppad_private/n_random_ > 0/hes_ran_/$$.
-$lnext
-If $icode%n_random_ > 0%$$,
-the member variable $codei%ran_obj_2_%$$ is the second order
-$cref/random objective/mixed_cppad_private/n_random_ > 0/ran_obj_2_/$$.
-$lnext
-If $icode%n_random_ > 0%$$,
-the member variable
-$cref/hes_fix_/mixed_cppad_private/n_random_ > 0/hes_fix_/$$.
-$lnext
-The member variable
-$cref/fix_like_/mixed_cppad_private/fix_like_/$$.
-$lnext
-The member variable
-$cref/initialize_done_/mixed_cppad_private/initialize_done_/$$
-must be false when this routine is called and will be true when it returns.
-$lend
 
 $end
 */
@@ -254,15 +198,18 @@ std::map<std::string, size_t> mixed_cppad::initialize(
 
 	// return value
 	std::map<std::string, size_t> size_map;
-	size_map["fix_like"]     = fix_like_.size_var();
-	size_map["a0_ran_like"]  = a0_ran_like_.size_var();
-	size_map["a1_ran_like"]  = a1_ran_like_.size_var();
-	size_map["ran_obj_2"]    = ran_obj_2_.size_var();
-	size_map["hes_ran"]      = hes_ran_row_.size();
-	size_map["newton_atom"]  = newton_atom_.size_var();
-	size_map["hes_fix"]      = hes_fix_row_.size();
-	size_t thread            = CppAD::thread_alloc::thread_num();
-	size_map["num_bytes"]    = CppAD::thread_alloc::inuse(thread);
+	size_map["hes_ran_row"]   = hes_ran_row_.size();
+	size_map["hes_cross_row"] = hes_cross_row_.size();
+	size_map["a0_ran_like"]   = a0_ran_like_.size_var();
+	size_map["a1_ran_like"]   = a1_ran_like_.size_var();
+	size_map["hes_ran_0"]     = hes_ran_0_.size_var();
+	size_map["newton_atom"]   = newton_atom_.size_var();
+	size_map["ran_obj_2"]     = ran_obj_2_.size_var();
+	size_map["fix_like"]      = fix_like_.size_var();
+	size_map["constraint"]    = constraint_.size_var();
+	//
+	size_t thread             = CppAD::thread_alloc::thread_num();
+	size_map["num_bytes"]     = CppAD::thread_alloc::inuse(thread);
 	return size_map;
 }
 
