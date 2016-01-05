8f3cede15919f7c3aa6798e044900e8092f3a167
Brad Bell <bradbell@seanet.com>
2016-01-05 09:59:55 -0700

    Advance to version 20160105.

--- bin/setup.py
+++ bin/setup.py
@@ -1,7 +1,7 @@
 # $Id:$
 #  --------------------------------------------------------------------------
 # dismod_at: Estimating Disease Rates as Functions of Age and Time
-#           Copyright (C) 2014-15 University of Washington
+#           Copyright (C) 2014-16 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
@@ -18,7 +18,7 @@ setup(
 	author  = 'Bradley M. Bell',
 	author_email= 'bradbell@uw.edu',
 	url='http://moby.ihme.washington.edu/bradbell/dismod_at',
-	version = '20151229',
+	version = '20160105',
 	packages = ['dismod_at' ],
 	package_dir = { 'dismod_at' : 'python/dismod_at' },
 	scripts     = list(),
