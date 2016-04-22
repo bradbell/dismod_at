aeb6d37a1da2533c8ef56d833f4307f34578223d
Brad Bell <bradbell@seanet.com>
2016-04-22 05:47:55 -0700

    scale_opt branch: fixed_effect.hpp: white space edit.

--- include/dismod_at/fixed_effect.hpp
+++ include/dismod_at/fixed_effect.hpp
@@ -1,7 +1,7 @@
 // $Id$
 /* --------------------------------------------------------------------------
 dismod_at: Estimating Disease Rates as Functions of Age and Time
-          Copyright (C) 2014-15 University of Washington
+          Copyright (C) 2014-16 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -23,15 +23,15 @@ namespace dismod_at {
 	//
 	template <class Float>
 	extern void get_fixed_effect(
-		const pack_info&             pack_object  ,
-		const CppAD::vector<Float>& pack_vec   ,
+		const pack_info&            pack_object  ,
+		const CppAD::vector<Float>& pack_vec     ,
 		CppAD::vector<Float>&       fixed_vec
 	);
 	//
 	template <class Float>
 	extern void put_fixed_effect(
-		const pack_info&             pack_object  ,
-		CppAD::vector<Float>&       pack_vec   ,
+		const pack_info&            pack_object  ,
+		CppAD::vector<Float>&       pack_vec     ,
 		const CppAD::vector<Float>& fixed_vec
 	);
 }
