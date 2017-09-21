cdbe1c3e9c617cf60dbced9f691af2017e722493
Brad Bell <bradbell@uw.edu>
2017-09-21 04:58:57 -0700

    build_type.sh: white space edits. avg_integrand.omh: Add missing susceptible and withC integrands.

--- bin/build_type.sh
+++ bin/build_type.sh
@@ -2,7 +2,7 @@
 # $Id:$
 #  --------------------------------------------------------------------------
 # dismod_at: Estimating Disease Rates as Functions of Age and Time
-#           Copyright (C) 2014-16 University of Washington
+#           Copyright (C) 2014-17 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
@@ -16,9 +16,9 @@ bin/build_type.sh program_name install_prefix build_type
 sets up soft links for the following directories:
 	install_prefix -> install_prefix.build_type
 	build          -> build.built_type
-program_name: is the program name for error reporting.
-install_prefix: This directory must end with /dismod_at
-build_type: This must be either debug or release
+program_name:   is the program name for error reporting.
+install_prefix: this directory must end with /dismod_at
+build_type:     this must be either debug or release
 EOF
 	exit 1
 fi
@@ -38,8 +38,8 @@ then
 	exit 1
 fi
 dollar='$'
-total_prefix=`echo $install_prefix |
-	sed -e "s|/dismod_at$dollar|/dismod_at.$build_type|"`
+total_prefix=`echo $install_prefix | sed \
+	-e "s|/dismod_at$dollar|/dismod_at.$build_type|" `
 if [ "$total_prefix" == "$install_prefix" ]
 then
 	echo "$program_name: install prefix does not end in /dismod_at"
