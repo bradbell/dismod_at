61698b42b04f5b06e1a94f7901efdf59ebea0f40
Brad Bell <bradbell@uw.edu>
2017-10-09 07:18:38 -0700

    atomic_speed.sh: make sure executable is built before creating database.

--- bin/atomic_speed.sh
+++ bin/atomic_speed.sh
@@ -9,35 +9,43 @@
 #	     GNU Affero General Public License version 3.0 or later
 # see http://www.gnu.org/licenses/agpl.txt
 # ---------------------------------------------------------------------------
-# Under Construction
-# ---------------------------------------------------------------------------
+# Parameters that can be changed
 random_seed='1234'
 n_children='20'
 n_data_per_child='40'
 quasi_fixed='false'
 # ---------------------------------------------------------------------------
+# convert to release build
 sed -i bin/run_cmake.sh \
 	-e 's|^build_type=.*|build_type=release|'
 bin/run_cmake.sh
-#
+# ---------------------------------------------------------------------------
+# create database
+cd build
+echo_eval make dismod_at
+cd ..
 echo_eval python3 example/user/speed.py \
 	$random_seed $n_children $n_data_per_child $quasi_fixed
 # ---------------------------------------------------------------------------
-#
+# locations relative to build directory
 database='example/user/example.db'
 program='devel/dismod_at'
 #
-for atomic in no yes
+for atomic in yes no
 do
+	# install cppad_mixed with this setting for atomic cholesky
 	sed -i bin/install_cppad_mixed.sh \
 		-e "s|^use_atomic_cholesky=.*|use_atomic_cholesky=$atomic|"
+	#
 	bin/install_cppad_mixed.sh
 	cd build
 	make dismod_at
 	#
+	# timing test
 	echo "(time $program $database fit both 2> time.$atomic"
 	(time $program $database fit both 0) 2> time.$atomic
 	#
+	# memory test
 	echo_eval massif.sh $program $database fit both 0
 	echo_eval mv massif.out massif.$atomic
 	#
