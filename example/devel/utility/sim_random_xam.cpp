e7cf725533fdd855711678c9e1a881f5346e1f40
Brad Bell <bradbell@uw.edu>
2017-10-13 21:20:12 -0700

    sim_random_xam.cpp: test students and log_students.

--- example/devel/utility/sim_random_xam.cpp
+++ example/devel/utility/sim_random_xam.cpp
@@ -30,6 +30,7 @@ $end
 # include <dismod_at/get_density_table.hpp>
 # include <cppad/mixed/manage_gsl_rng.hpp>
 # include <dismod_at/sim_random.hpp>
+# include <gsl/gsl_cdf.h>
 
 bool sim_random_xam(void)
 {	bool ok = true;
@@ -38,7 +39,7 @@ bool sim_random_xam(void)
 	// Initialize random number generator using the clock
 	size_t random_seed = CppAD::mixed::new_gsl_rng(1436703881);
 	//
-	size_t sample_size = 10000;
+	size_t sample_size = 100*100*100;
 	// -------------------------------------------------------------------
 	// check Gausian
 	dismod_at::density_enum density = dismod_at::gaussian_enum;
@@ -95,6 +96,34 @@ bool sim_random_xam(void)
 	check = exp( - std::sqrt(2.0) * 0.5 ) / 2.0;
 	ok   &= std::fabs( samp_prob / check - 1.0 ) < 1e-1;
 	// -------------------------------------------------------------------
+	// check Student's-t
+	density = dismod_at::students_enum;
+	delta   = 0.5;
+	mu      = 0.0;
+	count   = 0;
+	sum_z   = 0.0;
+	sum_zsq = 0.0;
+	nu      = 7.0;
+	for(size_t i = 0; i < sample_size; i++)
+	{	double z = dismod_at::sim_random(density, mu, delta, eta, nu);
+		if( 0.5 <= z / delta )
+			count ++;
+		sum_z   += z;
+		sum_zsq += z * z;
+	}
+	samp_mean = sum_z / double(sample_size);
+	samp_var  = sum_zsq / double(sample_size);
+	samp_prob = double(count) / double(sample_size);
+	//
+	check = 0.0;
+	ok   &= std::fabs(samp_mean - mu) < 2.0 / std::sqrt( double(sample_size) );
+	check = delta * delta;
+	ok   &= std::fabs(samp_var / check - 1.0) < 1e-1;
+	//
+	// integral from 0.5 to infinity of p(z) dz
+	check = gsl_cdf_tdist_Q(0.5, nu);
+	ok   &= std::fabs( samp_prob / check - 1.0 ) < 1e-1;
+	// -------------------------------------------------------------------
 	// check Log-Gausian
 	density        = dismod_at::log_gaussian_enum;
 	delta          = 1e-1;
@@ -149,6 +178,36 @@ bool sim_random_xam(void)
 	ok   &= std::fabs(samp_var - 1.0) < 1e-1;
 	// integral from 0.5 to infinity of p(z) dz
 	check = exp( - std::sqrt(2.0) * 0.5 ) / 2.0;
+	// std::cout << samp_prob / check << std::endl;
+	ok   &= std::fabs( samp_prob / check - 1.0 ) < 1e-1;
+	// -------------------------------------------------------------------
+	// check Log-Student's-t
+	density = dismod_at::log_students_enum;
+	delta   = 1e-1;
+	mu      = 1e-1;
+	eta     = 1e-2;
+	nu      = 7.0;
+	count   = 0;
+	sum_w   = 0.0;
+	sum_wsq = 0.0;
+	sigma   = log(mu + eta + delta) - log(mu + eta);
+	for(size_t i = 0; i < sample_size; i++)
+	{	double z     = dismod_at::sim_random(density, mu, delta, eta, nu);
+		double w     = ( log(z + eta) - log(mu + eta) ) / sigma;
+		if( 0.5 <= w )
+			count ++;
+		sum_w   += w;
+		sum_wsq += w * w;
+	}
+	samp_mean = sum_w / double(sample_size);
+	samp_var  = sum_wsq / double(sample_size);
+	samp_prob = double(count) / double(sample_size);
+	//
+	check = 0.0;
+	ok   &= std::fabs(samp_mean) < 3.0 / std::sqrt( double(sample_size) );
+	ok   &= std::fabs(samp_var - 1.0) < 1e-1;
+	// integral from 0.5 to infinity of p(z) dz
+	check = gsl_cdf_tdist_Q(0.5, nu);
 	// std::cout << samp_prob / check << std::endl;
 	ok   &= std::fabs( samp_prob / check - 1.0 ) < 1e-1;
 	// ---------------------------------------------------------------------
