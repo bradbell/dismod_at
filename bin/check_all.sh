e7c53c3714b6079b4d34e57ebe840cb67fde7744
Brad Bell <bradbell@seanet.com>
2018-01-17 06:32:41 -0700

    check_all.sh: separate std::cout and std::cerr into *.log and  *.err.

--- bin/check_all.sh
+++ bin/check_all.sh
@@ -2,7 +2,7 @@
 # $Id:$
 #  --------------------------------------------------------------------------
 # dismod_at: Estimating Disease Rates as Functions of Age and Time
-#           Copyright (C) 2014-17 University of Washington
+#           Copyright (C) 2014-18 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
@@ -18,40 +18,48 @@ then
 	echo 'must be executed from its parent directory'
 	exit 1
 fi
-# ---------------------------------------------------------------------------
-if [ "$1" != '+speed' ] && [ "$1" != '-speed' ]
-then
-	echo 'check_all.sh: option is not +speed or -speed'
-	exit 1
-fi
+# -----------------------------------------------------------------------------
+# bash function that echos and executes a command
+echo_eval() {
+	echo $*
+	eval $*
+}
+# -----------------------------------------------------------------------------
 bin/version.sh check
 bin/check_devel_xam.sh
 bin/check_include.sh
 bin/check_srcfile.sh
 bin/check_configure.sh
 #
+# check latex in omhelp
 run_omhelp.sh -xml doc
-bin/run_cmake.sh
 # ----------------------------------------------------------------------------
+echo 'bin/run_cmake.sh 1> cmake.log 2> cmake.err'
+bin/run_cmake.sh 1> cmake.log 2> cmake.err
+#
 cd build
-echo "make check >& check_all.log"
-if ! make check >& ../check_all.log
-then
-	echo 'check_all.sh: make check failed, see check_all.log'
-	exit 1
-fi
-if grep ': *warning *:' ../check_all.log
-then
-	echo 'check_all.sh: aborting: see compiler warnings in check_all.log'
-	exit 1
-fi
-if [ "$input" == '+speed' ]
-then
-	make speed
-fi
-# ----------------------------------------------------------------------------
+#
+echo 'make check 1> check.log 2> check.err'
+make check 1> check.log 2> ../check.err
+#
+echo 'make speed 1> speed.log 2> speed.err'
+make speed 1> speed.log 2> ../speed.err
+#
 cd ..
 #
-bin/check_install.sh
+echo 'bin/check_install.sh 1> install.log 2> install.err'
+bin/check_install.sh 1> install.log 2> install.err
+# -----------------------------------------------------------------------------
+for target in cmake check speed install
+do
+	err=`cat $target.err`
+	if [ "$err" != '' ]
+	then
+		cat $target.err
+		echo "bin/run_check_all.sh: $target.err is not empty."
+		exit 1
+	fi
+	echo_eval rm $target.err
+done
 # -----------------------------------------------------------------------------
 echo 'check_all.sh: OK'
