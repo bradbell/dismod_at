fc1ea8b352d8603059a0a0c6d59a286983823fdf
Brad Bell <bradbell@seanet.com>
2018-09-30 05:12:23 -0700

    master: speed.sh: change default settings, change file name in echo.

--- bin/speed.sh
+++ bin/speed.sh
@@ -15,13 +15,13 @@
 random_seed='123'
 #
 # number of random effects is 2 * n_children
-n_children='10'
+n_children='20'
 #
 # use quasi-Newton (otherwise Newton)
 quasi_fixed='false'
 #
 # smaller increase work per function evaluation
-ode_step_size='5.0'
+ode_step_size='1.0'
 #
 # this branch name must not be empty
 branch1='master'
@@ -98,7 +98,7 @@ do
 	sed -i bin/run_cmake.sh -e "s|^build_type=.*|build_type='release'|"
 	#
 	# run cmake
-	echo "bin/run_cmake.sh > build/$name.log"
+	echo "bin/run_cmake.sh > build.release/$name.log"
 	bin/run_cmake.sh > build/$name.log
 	#
 	if [ "$name" != 'master' ]
@@ -110,14 +110,14 @@ do
 		# install cppad
 		if [ "$install_cppad" == 'true' ]
 		then
-			echo "bin/install_cppad.sh >> build/$name.log"
+			echo "bin/install_cppad.sh >> build.release/$name.log"
 			bin/install_cppad.sh >> build/$name.log
 		fi
 		#
 		# install cppad_mixed
 		if [ "$install_cppad_mixed" == 'true' ]
 		then
-			echo "bin/install_cppad_mixed.sh >> build/$name.log"
+			echo "bin/install_cppad_mixed.sh >> build.release/$name.log"
 			bin/install_cppad_mixed.sh >> build/$name.log
 		fi
 	fi
@@ -127,7 +127,7 @@ do
 	# build dismod_at
 	cd build
 	make clean
-	echo "make dismod_at >> build/$name.log"
+	echo "make dismod_at >> build.release/$name.log"
 	make dismod_at >> $name.log
 	cd ..
 	# ------------------------------------------------------------------------
@@ -136,7 +136,7 @@ do
 	#
 	# create database
 	arguments="$random_seed $n_children $quasi_fixed $ode_step_size"
-	echo "python3 example/user/speed.py $arguments >> build/$name.log"
+	echo "python3 example/user/speed.py $arguments >> build.release/$name.log"
 	python3 example/user/speed.py $arguments >> build/$name.log
 	#
 	# change into database directory
@@ -144,14 +144,14 @@ do
 	#
 	# run timing test
 	program="../../devel/dismod_at"
-	echo "time $program example.db fit both 0 >& build/$name.out"
+	echo "time $program example.db fit both 0 >& build.release/$name.out"
 	( time $program example.db fit both 0 ) >& ../../$name.out
 	#
 	# run memory test
 	echo "massif.sh $program example.db fit both 0 >& /dev/null"
 	massif.sh $program example.db fit both 0 >& /dev/null
 	#
-	echo "mv massif.out build/$name.massif"
+	echo "mv massif.out build.release/$name.massif"
 	mv massif.out ../../$name.massif
 	#
 	popd > /dev/null
@@ -170,14 +170,14 @@ then
 	# install cppad
 	if [ "$install_cppad" == 'true' ]
 	then
-		echo "bin/install_cppad.sh >> build/$name.log"
+		echo "bin/install_cppad.sh >> build.release/$name.log"
 		bin/install_cppad.sh >> build/$name.log
 	fi
 	#
 	# install cppad_mixed
 	if [ "$install_cppad_mixed" == 'true' ]
 	then
-		echo "bin/install_cppad_mixed.sh >> build/$name.log"
+		echo "bin/install_cppad_mixed.sh >> build.release/$name.log"
 		bin/install_cppad_mixed.sh >> build/$name.log
 	fi
 fi
