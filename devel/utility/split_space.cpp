a45a713b2cb3e7ac6199d1c9d8f88953c68d1ccf
Brad Bell <bradbell@seanet.com>
2018-10-13 16:19:15 -0700

    ode: get_option_table_xam.cpp: fix by adding ode_age_split test. split_space.cpp: document empty result case.

--- devel/utility/split_space.cpp
+++ devel/utility/split_space.cpp
@@ -1,7 +1,7 @@
 // $Id:$
 /* --------------------------------------------------------------------------
 dismod_at: Estimating Disease Rates as Functions of Age and Time
-          Copyright (C) 2014-17 University of Washington
+          Copyright (C) 2014-18 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -31,6 +31,11 @@ $head vec$$
 This is the resulting vector of non-empty substrings.
 If all the characters in $icode str$$ are spaces,
 the resulting vector with have size zero.
+
+$head empty$$
+If $icode str$$ only contains spaces,
+the return vector is empty; i.e.
+$icode%vec%.size() == 0%$$.
 
 
 $children%example/devel/utility/split_space_xam.cpp
