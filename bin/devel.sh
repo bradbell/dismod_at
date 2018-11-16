535c904a9b469702f7d049e281dd4693296eb059
Brad Bell <bradbell@seanet.com>
2018-11-16 10:49:58 -0800

    master: doc.omh: change links to current page to use new jump table option.
    devel.sh: new configuration files used by ~/devel tools.

---
+++ bin/devel.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+# dismod_at: Estimating Disease Rates as Functions of Age and Time
+#           Copyright (C) 2014-18 University of Washington
+#              (Bradley M. Bell bradbell@uw.edu)
+#
+# This program is distributed under the terms of the
+#	     GNU Affero General Public License version 3.0 or later
+# see http://www.gnu.org/licenses/agpl.txt
+# -----------------------------------------------------------------------------
+# configuration file for tools in ~/devel.
+#
+# web address linked by run_omhelp.sh to the Home icon on each page.
+image_link='https://bradbell.github.io/dismod_at'
+echo "image_link=$image_link"
+#
+# specialfiles that check_copyright.sh should ignore:
+copyright_ignore='
+'
