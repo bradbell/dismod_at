11f5e7aba12001acbeb075fa0f0a05f7495cd36d
Brad Bell <bradbell@seanet.com>
2019-04-18 09:46:26 -0700

    update gh-pages to version 20190418

--- doc/_childtable_command_htm.js
+++ doc/_childtable_command_htm.js
@@ -5,9 +5,9 @@ document.write('\
 <option>set_command</option>\
 <option>init_command</option>\
 <option>depend_command</option>\
+<option>sample_command</option>\
 <option>fit_command</option>\
 <option>simulate_command</option>\
-<option>sample_command</option>\
 <option>predict_command</option>\
 <option>dismodat.py</option>\
 </select>\
@@ -17,9 +17,9 @@ function command_child(item)
 		'set_command.htm',
 		'init_command.htm',
 		'depend_command.htm',
+		'sample_command.htm',
 		'fit_command.htm',
 		'simulate_command.htm',
-		'sample_command.htm',
 		'predict_command.htm',
 		'dismodat.py.htm'
 	];
