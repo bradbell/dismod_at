bbbca84a719b9503715532c93412870e237d2ed4
Brad Bell <bradbell@seanet.com>
2019-04-24 05:13:34 -0700

    update gh-pages to version 20190424

--- doc/_childtable_command_htm.js
+++ doc/_childtable_command_htm.js
@@ -2,27 +2,27 @@
 document.write('\
 <select onchange="command_child(this)">\
 <option>command-&gt;</option>\
-<option>old2new_command</option>\
-<option>set_command</option>\
-<option>init_command</option>\
 <option>depend_command</option>\
+<option>init_command</option>\
+<option>old2new_command</option>\
+<option>predict_command</option>\
 <option>sample_command</option>\
+<option>set_command</option>\
 <option>fit_command</option>\
 <option>simulate_command</option>\
-<option>predict_command</option>\
 <option>dismodat.py</option>\
 </select>\
 ');
 function command_child(item)
 {	var child_list = [
-		'old2new_command.htm',
-		'set_command.htm',
-		'init_command.htm',
 		'depend_command.htm',
+		'init_command.htm',
+		'old2new_command.htm',
+		'predict_command.htm',
 		'sample_command.htm',
+		'set_command.htm',
 		'fit_command.htm',
 		'simulate_command.htm',
-		'predict_command.htm',
 		'dismodat.py.htm'
 	];
 	var index = item.selectedIndex;
