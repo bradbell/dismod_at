915d745ea225bf2864e75c70e6588dc4f4e0ff7d
Brad Bell <bradbell@seanet.com>
2019-04-25 07:04:45 -0700

    update gh-pages to version 20190425

--- doc/_search.htm.js
+++ doc/_search.htm.js
@@ -3,7 +3,7 @@
 // ------------------------------------------------------------ 
 Keyword = 
 [
-'dismod_at  Disease Rates as Functions of Age and Time: dismod_at-20190424  ',' links ',
+'dismod_at  Disease Rates as Functions of Age and Time: dismod_at-20190425  ',' links ',
 'math_abstract  An Introduction To The Mathematics of Dismod_at  ',' ',
 'math_ode  The Dismod_at Ordinary Differential Equation  ',' susceptible s(a t) with condition c(a prevalence p(a incidence iota(a remission rho(a excess mortality chi(a other cause omega(a initial pini(t) ',
 'censor_neg  The Distributions With Censored Negative Values  ',' under construction reference discussion simulation test gaussian density error function probability g_+ laplace indefinite integral l_+ ',
@@ -100,7 +100,7 @@ Keyword =
 'random_value  The Random Effects Value Density Function  ',' j k_j a_j t_j u_cj lambda_j delta_j d_j mu_j v_cj^u v^u ',
 'random_diff  Random Effects Difference Density Function  ',' k i(k) j(k) lambda_k u_ijkc delta_ijk d_ijk mu_ijk age differences a^u time t^u ',
 'random_prior  Prior for Random Effect Values  ',' p(u|theta) ',
-'statistic  Some Statistical Function Definitions  ',' notation log-transformed standard deviation sigma weighted residual value difference reversed uniform gaussian laplace student\'s-t log-gaussian log-laplace log-student\'s-t log-density ',
+'statistic  Some Statistical Function Definitions  ',' notation z y mu delta eta nu log-transformed standard deviation sigma weighted residual value difference reversed log-density uniform gaussian log-gaussian laplace log-laplace student\'s-t log-student\'s-t ',
 'bilinear  dismod_at Bilinear Interpolation  ',' age grid time function interpolant below minimum above maximum outside both limits plotting ',
 'posterior  Simulating Posterior Distribution for Model Variables  ',' purpose lemma 1 proof remark 2 simulation ',
 'prev_dep  Prevalence Does Not Depend On Other Cause Mortality  ',' lemma proof ',
@@ -285,12 +285,12 @@ Keyword =
 'avg_noise_effect_ctor  Constructing An Average Noise Effect Object  ',' syntax prototype ode_step_size age_avg_grid age_table time_table integrand_table w_info_vec s_info_vec pack_object example ',
 'avg_noise_effect_rectangle  Computing One Average Integrand  ',' syntax prototype age_lower age_upper time_lower time_upper weight_id integrand_id float pack_vec example ',
 'avg_noise_effect_xam.cpp  C++ avg_noise_effect: Example and Test  ',' ',
-'whats_new_2019  Changes and Additions to Dismod_at During 2019  ',' 04-24 04-23 04-22 04-21 04-20 04-18 04-17 04-15 04-14 04-12 04-11 04-10 04-09 04-08 04-06 02-16 01-26 01-23 01-22 01-16 01-15 01-10 ',
+'whats_new_2019  Changes and Additions to Dismod_at During 2019  ',' 04-25 04-24 04-23 04-22 04-21 04-20 04-18 04-17 04-15 04-14 04-12 04-11 04-10 04-09 04-08 04-06 02-16 01-26 01-23 01-22 01-16 01-15 01-10 ',
 'whats_new_2018  Changes and Additions to Dismod_at During 2018  ',' 12-19 12-15 12-14 10-30 10-22 10-21 10-13 10-05 09-30 09-28 09-24 09-14 08-30 08-29 08-22 08-13 08-09 08-08 08-07 08-06 07-31 07-30 07-27 07-26 api other 07-25 07-23 07-21 07-13 06-30 06-29 06-21 06-20 06-18 06-14 06-09 06-04 05-24 05-23 05-21 05-14 05-09 05-08 05-07 05-05 05-04 05-02 04-30 04-26 04-25 04-24 04-23 04-22 04-21 04-16 04-13 04-12 04-11 04-10 04-09 04-08 04-07 04-06 03-21 03-13 03-10 03-08 03-06 02-20 02-17 02-14 02-13 02-12 02-11 02-09 02-08 02-05 02-04 02-03 02-02 02-01 01-31 01-30 01-28 01-26 01-25 01-24 01-23 01-22 01-21 01-20 01-18 01-16 01-12 01-09 ',
 'whats_new_2017  Changes and Additions to Dismod_at During 2017  ',' 12-30 12-22 12-21 12-20 12-16 12-13 12-10 11-12 11-08 10-25 10-24 10-22 10-21 10-20 10-18 10-17 10-14 10-13 10-09 10-06 10-05 10-04 10-02 10-01 09-30 09-29 09-28 09-27 09-26 09-25 09-23 09-21 09-20 09-19 09-18 09-17 09-16 09-02 09-01 08-30 08-07 05-23 05-05 05-02 05-01 04-29 04-27 04-26 04-25 04-24 04-23 04-22 04-21 04-20 04-19 04-18 04-17 04-16 04-15 04-09 04-07 04-05 04-04 04-03 04-01 03-30 03-29 03-17 03-06 03-04 03-03 01-27 01-26 01-24 01-14 01-15 ',
 'whats_new_2016  Changes and Additions to Dismod_at During 2016  ',' 12-03 10-31 10-20 10-19 10-18 10-17 10-16 10-15 10-14 10-13 10-12 10-11 09-30 09-29 09-21 08-28 07-29 07-28 07-15 07-14 07-11 06-13 06-12 06-06 06-05 05-16 05-15 05-11 05-10 05-09 05-08 05-07 05-06 05-05 05-04 05-03 05-01 04-28 04-27 04-24 04-23 04-22 04-21 04-20 04-19 04-18 04-17 04-11 04-09 04-07 03-29 02-26 02-06 01-26 01-16 01-15 01-10 01-06 01-05 ',
 'whats_new_2015  Changes and Additions to Dismod_at During 2015  ',' 12-29 12-24 12-15 12-14 12-10 12-09 11-23 11-22 11-21 11-20 11-17 11-16 11-15 11-14 11-12 11-10 11-09 11-07 11-06 11-05 11-04 11-03 11-02 11-01 10-30 10-29 10-28 10-27 10-26 10-25 10-24 10-20 10-19 10-16 10-15 10-14 09-18 09-03 08-31 08-27 08-21 08-18 08-15 08-14 08-13 08-12 08-09 08-08 08-04 08-03 08-02 08-01 07-29 07-28 07-27 07-25 07-24 07-23 07-22 07-21 07-20 07-19 07-17 07-16 07-15 07-14 07-13 07-11 07-10 07-09 07-08 07-07 07-05 07-04 07-03 07-01 06-30 06-29 06-27 06-26 06-25 06-24 06-23 06-22 ',
-'wish_list  Dismod_at Wish List  ',' ode solution prevalence large excess mortality csv2db command covariate multipliers random effects grids value priors difference sample conditionals checkpointing weighting meas_std diagrams lagrange documentation real world example user examples model sampling starting tracing optimization windows install '
+'wish_list  Dismod_at Wish List  ',' ode solution prevalence large excess mortality csv2db command covariate multipliers random effects grids value priors difference sample conditionals checkpointing weighting meas_std diagrams lagrange documentation real world example user examples sampling starting tracing optimization windows install '
 ]
 
 var MaxList = 100;
