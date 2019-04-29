bdb60ae1669d9e474570222b5554e72883a6d4e0
Brad Bell <bradbell@seanet.com>
2019-04-29 09:57:04 -0700

    master:

--- example/user/censor.py
+++ example/user/censor.py
@@ -65,6 +65,12 @@
 # There is one prior in the $cref prior_table$$ for the only
 # model variable.
 #
+# $head Discussion$$
+# This fits two data sets.
+# The first is simulated using python and put in the $cref data_table$$.
+# The second is simulated using the $cref simulate_command$$
+# and put in the $cref data_sim_table$$.
+#
 # $head Source Code$$
 # $srcfile%
 #	example/user/censor.py
@@ -217,6 +223,7 @@ def example_db (file_name) :
 	# ----------------------------------------------------------------------
 	# option_table
 	option_table = [
+		{ 'name':'random_seed',            'value':str(random_seed)    },
 		{ 'name':'parent_node_name',       'value':'world'             },
 		{ 'name':'rate_case',              'value':'iota_pos_rho_zero' },
 
@@ -246,11 +253,14 @@ def example_db (file_name) :
 	)
 	# ----------------------------------------------------------------------
 	return
-# ===========================================================================
-# Fit to estimate iota
+# ----------------------------------------------------------------------------
+# Fit to estimate iota from censored data
+# ----------------------------------------------------------------------------
+#
 file_name = 'example.db'
 example_db(file_name)
 #
+# init and fit fixed
 program = '../../devel/dismod_at'
 system_command([ program, file_name, 'init' ])
 system_command([ program, file_name, 'fit', 'fixed' ])
@@ -261,12 +271,44 @@ connection      = dismod_at.create_connection(file_name, new)
 var_table       = dismod_at.get_table_dict(connection, 'var')
 fit_var_table   = dismod_at.get_table_dict(connection, 'fit_var')
 #
+# check result of the fit fixed
 assert len(var_table) == 1
-estimate   = fit_var_table[0]['fit_var_value']
-if abs( 1.0 - estimate / iota_true ) > 1e-1 :
+first_estimate = fit_var_table[0]['fit_var_value']
+if abs( 1.0 - first_estimate / iota_true ) > 1e-1 :
 	print("random_seed = ", random_seed)
-	print(1.0 - estimate / iota_true)
+	print(1.0 - first_estimate / iota_true)
 	assert False
+# -----------------------------------------------------------------------------
+# Simulate a censored data set and fit it
+# ----------------------------------------------------------------------------
+# set truth_var table to contain iota_true
+tbl_name = 'truth_var'
+col_name = [ 'truth_var_value' ]
+col_type = [ 'real' ]
+row_list = [ [iota_true] ]
+dismod_at.create_table(connection, tbl_name, col_name, col_type, row_list)
+#
+# simulate a new data set and sample using it
+system_command([ program, file_name, 'set', 'avgint', 'data' ])
+system_command([ program, file_name, 'simulate', '1' ])
+system_command([ program, file_name, 'fit', 'fixed', '0' ])
+#
+# check result of the second fit fixed
+connection       = dismod_at.create_connection(file_name, new)
+fit_var_table    = dismod_at.get_table_dict(connection, 'fit_var')
+second_estimate  = fit_var_table[0]['fit_var_value']
+if abs( 1.0 - second_estimate / iota_true ) > 1e-1 :
+	print("random_seed = ", random_seed)
+	print(1.0 - second_estimate / iota_true)
+	assert False
+#
+# check that the estimates were different; i,e., used the second data set
+assert abs( (first_estimate - second_estimate) / iota_true ) > 1e-2
+#
+# check all the simulated data values were non-negative
+data_sim_table = dismod_at.get_table_dict(connection, 'data_sim')
+for row in data_sim_table :
+	assert row['data_sim_value'] >= 0.0
 # -----------------------------------------------------------------------------
 print('censor.py: OK')
 # -----------------------------------------------------------------------------
