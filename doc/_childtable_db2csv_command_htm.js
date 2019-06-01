1fdd8100c15a363bc6332c2e99cffe4f560f1a11
Brad Bell <bradbell@seanet.com>
2019-06-01 09:29:44 -0700

    update gh-pages to version 20190601

--- doc/_childtable_db2csv_command_htm.js
+++ doc/_childtable_db2csv_command_htm.js
@@ -3,11 +3,13 @@ document.write('\
 <select onchange="db2csv_command_child(this)">\
 <option>db2csv_command-&gt;</option>\
 <option>db2csv_command.py</option>\
+<option>ihme_db.sh</option>\
 </select>\
 ');
 function db2csv_command_child(item)
 {	var child_list = [
-		'db2csv_command.py.htm'
+		'db2csv_command.py.htm',
+		'ihme_db.sh.htm'
 	];
 	var index = item.selectedIndex;
 	item.selectedIndex = 0;
