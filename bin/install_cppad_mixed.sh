8b3562ad14e0a976e504ce7ed047e21d9b6b0de4
Brad Bell <bradbell@seanet.com>
2019-07-20 09:45:13 -0700

    master: advance to cppad_mixed-20190720.

--- bin/install_cppad_mixed.sh
+++ bin/install_cppad_mixed.sh
@@ -23,8 +23,8 @@ echo_eval() {
 }
 # --------------------------------------------------------------------------
 web_page='https://github.com/bradbell/cppad_mixed.git'
-hash_key='addbe2807349d9fcf67ea759e5957964093ae13d'
-version='20190710'
+hash_key='d153ede918cc3a6580369771fde80767862c23cc'
+version='20190720'
 # ---------------------------------------------------------------------------
 # special cppad_mixed settings (it is not suggested that you change these)
 #
