728f2f9a32b8de7ade1694af033198c20a1d4da9
Brad Bell <bradbell@seanet.com>
2019-09-12 17:26:30 -0700

    update gh-pages to version 20190912

--- doc/_search.htm.js
+++ doc/_search.htm.js
@@ -19,7 +19,7 @@ Keyword =
 'user_fit_random.py  Fitting Just Random Effects  ',' purpose discussion source code ',
 'user_fit_fixed_both.py  Fit Fixed First Then Both  ',' purpose discussion source code ',
 'user_posterior.py  Sampling From Then Posterior Distribution  ',' purpose discussion variance source code ',
-'user_fit_sim.py  Fitting Simulated Data Example  ',' parent iota child other rates covariate multiplier priors source code ',
+'user_fit_sim.py  Fitting Simulated Data Example  ',' parent iota child other rates covariate multiplier starting point scaling priors source code ',
 'user_fit_gamma.py  Fitting Measurement Noise Covariate Multipliers, Gamma  ',' random effects iota other rates data meas_noise_effect scaling source code ',
 'user_age_avg_split.py  Non-uniform Age Average Grid  ',' purpose variables prior data fit predict ode_step_size source code ',
 'user_diabetes.py  An Example / Speed Test Fitting Simulated Diabetes Data  ',' running this omega rho weight table grid covariates multipliers truth parent rates child effects predict problem parameters mulcov_dict node_list integrand_list parent_age_grid child_age_grid parent_time_grid child_time_grid ode_step_size meas_cv meas_repeat fit_with_noise_in_data random_seed quasi_fixed tolerance_fixed derivative_test_fixed truth2start accept_rel_err source code ',
