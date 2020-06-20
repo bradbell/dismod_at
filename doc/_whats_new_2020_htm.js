6e9cca2b5cf7b07baa24f5208e1b9749233901a6
Brad Bell <bradbell@seanet.com>
2020-06-20 10:44:10 -0700

    update gh-pages to version 20200620

--- doc/_whats_new_2020_htm.js
+++ doc/_whats_new_2020_htm.js
@@ -11,6 +11,8 @@ var list_across0 = [
 '_external.htm'
 ];
 var list_current0 = [
+'whats_new_2020.htm#06-20',
+'whats_new_2020.htm#06-08',
 'whats_new_2020.htm#05-30',
 'whats_new_2020.htm#05-29',
 'whats_new_2020.htm#05-27',
