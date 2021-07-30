07db375e4f48594cff16a90362f6a0663f523674
Brad Bell <bradbell@seanet.com>
2021-07-29 22:18:01 -0700

    update gh-pages to version 20210729

--- doc/_childtable_python_htm.js
+++ doc/_childtable_python_htm.js
@@ -3,8 +3,8 @@ document.write('\
 <select onchange="python_child(this)">\
 <option>python-&gt;</option>\
 <option>create_connection</option>\
-<option>create_table</option>\
 <option>create_database</option>\
+<option>create_table</option>\
 <option>csv2db_command</option>\
 <option>db2csv_command</option>\
 <option>get_name_type</option>\
@@ -12,6 +12,7 @@ document.write('\
 <option>get_table_dict</option>\
 <option>metropolis</option>\
 <option>modify_command</option>\
+<option>sim_data</option>\
 <option>sql_command</option>\
 <option>system_command_prc</option>\
 <option>unicode_tuple</option>\
@@ -20,8 +21,8 @@ document.write('\
 function python_child(item)
 {	var child_list = [
 		'create_connection.htm',
-		'create_table.htm',
 		'create_database.htm',
+		'create_table.htm',
 		'csv2db_command.htm',
 		'db2csv_command.htm',
 		'get_name_type.htm',
@@ -29,6 +30,7 @@ function python_child(item)
 		'get_table_dict.htm',
 		'metropolis.htm',
 		'modify_command.htm',
+		'sim_data.htm',
 		'sql_command.htm',
 		'system_command_prc.htm',
 		'unicode_tuple.htm'
--- doc/_contents.js
+++ doc/_contents.js
@@ -1,6 +1,6 @@
 var Cookie            = document.cookie;
 var Open              = '';
-var SectionCount      = 192   ;
+var SectionCount      = 194   ;
 var ImagesInitialized = false;
 Children              = new Array();
 Initialize();
