de26e4193bc68bae71b217ef0dd769dc0dc6c32c
Brad Bell <bradbell@seanet.com>
2021-08-25 15:49:11 -0700

    master: average_integrand.py: fix font in syntax.

--- python/dismod_at/average_integrand.py
+++ python/dismod_at/average_integrand.py
@@ -25,7 +25,7 @@ $head Under Construction$$
 This routine is under construction.
 
 $head Syntax$$
-$codei%avg_integrand% = dismod_at.average_integrand(
+$icode%avg_integrand% = dismod_at.average_integrand(
 	%rate%, %integrand_name%, %grid%, %abs_tol%
 )%$$
 
