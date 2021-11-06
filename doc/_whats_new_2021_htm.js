74823ac42c5d939baa255be225ca9c6bcb5e5c71
Brad Bell <bradbell@seanet.com>
2021-11-06 02:57:42 -0700

    update gh-pages to version 20211106

--- doc/_whats_new_2021_htm.js
+++ doc/_whats_new_2021_htm.js
@@ -11,6 +11,8 @@ var list_across0 = [
 '_external.htm'
 ];
 var list_current0 = [
+'whats_new_2021.htm#11-06',
+'whats_new_2021.htm#11-05',
 'whats_new_2021.htm#11-03',
 'whats_new_2021.htm#10-31',
 'whats_new_2021.htm#10-25',
