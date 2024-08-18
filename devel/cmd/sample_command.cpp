7b59e6462378028fa336c5713b66e2d7efabb41a
Brad Bell <bradbell@seanet.com>
2024-08-18 06:42:28 -0700

    master: remove include log_message.hpp where it is not used. log_table.xrst: include mention of non-zero exit on error.

--- devel/cmd/sample_command.cpp
+++ devel/cmd/sample_command.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: University of Washington <https://www.washington.edu>
-// SPDX-FileContributor: 2014-22 Bradley M. Bell
+// SPDX-FileContributor: 2014-24 Bradley M. Bell
 // ----------------------------------------------------------------------------
 
 # include <dismod_at/sample_command.hpp>
@@ -13,7 +13,6 @@
 # include <dismod_at/null_int.hpp>
 # include <dismod_at/get_var_limits.hpp>
 # include <dismod_at/remove_const.hpp>
-# include <dismod_at/log_message.hpp>
 # include <dismod_at/get_str_map.hpp>
 
 
--- devel/table/get_subgroup_table.cpp
+++ devel/table/get_subgroup_table.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: University of Washington <https://www.washington.edu>
-// SPDX-FileContributor: 2014-22 Bradley M. Bell
+// SPDX-FileContributor: 2014-24 Bradley M. Bell
 // ----------------------------------------------------------------------------
 /*
 {xrst_begin get_subgroup_table dev}
@@ -80,7 +80,6 @@ this function.
 # include <dismod_at/error_exit.hpp>
 # include <dismod_at/null_int.hpp>
 # include <dismod_at/exec_sql_cmd.hpp>
-# include <dismod_at/log_message.hpp>
 # include <cppad/utility/to_string.hpp>
 
 namespace dismod_at { // BEGIN DISMOD_AT_NAMESPACE
