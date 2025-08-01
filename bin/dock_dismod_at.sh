6ee963b999d0eedc5ebb206a422fd9e835c29227
Garland Culbreth <culbreth.garland@gmail.com>
2025-08-01 11:03:00 -0700

    test: update dock_dismod_at.sh hash

--- bin/dock_dismod_at.sh
+++ bin/dock_dismod_at.sh
@@ -156,7 +156,7 @@ set -e -u
 # {xrst_spell_off}
 # {xrst_code sh}
    dismod_at_version='2025.7.11'
-   dismod_at_hash='3dbb4d26eb57488df7e6a316759d32f70a154862'
+   dismod_at_hash='657f9f06a3d607fa52bd3345fab36ef37c223458'
 # {xrst_code}
 # {xrst_spell_on}
 #
